3b266ebbadc2dce0072abea4aaee14ee97e07501
Hevanafa <dhevansafa4@gmail.com>
2025-11-17 12:16:00 +0700

    added loadImage example

--- game.js
+++ game.js
@@ -10,7 +10,29 @@ const importObject = {
   }
 }
 
+async function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = url
+  })
+}
+
 async function main() {
+  // Load assets
+  const img = await loadImage("assets/images/satono_diamond.png");
+  console.log(`Loaded image: { w: ${img.width}, h: ${img.height} }`);
+
+  // Copy image
+  const tempCanvas = document.createElement("canvas");
+  tempCanvas.width = img.width;
+  tempCanvas.height = img.height;
+  const tempCtx = tempCanvas.getContext("2d");
+  tempCtx.drawImage(img, 0, 0);
+  const pixels = tempCtx.getImageData(0, 0, img.width, img.height).data;
+
+  // Init engine
   const response = await fetch("game.wasm");
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, importObject);
