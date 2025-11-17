68aae736bda3e7f3a3035111725b6492fd029e3f
Hevanafa <dhevansafa4@gmail.com>
2025-11-17 11:01:52 +0700

    renamed gradient to game in compile script

--- compile.ps1
+++ compile.ps1
@@ -1,7 +1,7 @@
 # Compile targetting wasm32-embedded
-E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded .\gradient.pas
+E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded .\game.pas
 
-if (test-path -path "gradient.wasm" -pathType leaf) {
-	del "gradient.wasm"
+if (test-path -path "game.wasm" -pathType leaf) {
+	del "game.wasm"
 }
-ren "gradient" "gradient.wasm"
+ren "game" "game.wasm"
