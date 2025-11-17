22b4954783f99550e565ee75b873d68efa556698
Hevanafa <dhevansafa4@gmail.com>
2025-11-17 15:06:08 +0700

    added bitmap.pas

--- game.pas
+++ game.pas
@@ -2,52 +2,7 @@ library Game;
 
 {$Mode ObjFPC}
 
-uses VGA;
-
-{ Bitmap operations }
-const
-  maxImageBufferSize = 128 * 128 * 4;
-
-var
-  imageBuffer: array[0..maxImageBufferSize - 1] of byte;
-
-function getImageBuffer: pointer;
-begin
-  getImageBuffer := @imageBuffer
-end;
-
-{
-function allocImageData(const size: integer): pointer;
-var
-  ptr: pointer;
-begin
-  Important: This always crashes on WebAssembly
-  getmem(ptr, size);
-  allocImageData := ptr
-end;
-}
-
-{ TODO: Use a PBitmap }
-procedure spr(const x, y, width, height: integer);
-var
-  a, b, srcPos, destPos: integer;
-  surface: PByteArray;
-begin
-  surface := PByteArray(getSurface);
-
-  for b:=0 to height - 1 do
-  for a:=0 to width - 1 do begin
-    srcPos := (b * width + a) * 4;
-    destPos := ((y + b) * vgaWidth + (x + a)) * 4;
-
-    { Order: RGBA }
-    surface^[destPos] := imageBuffer[srcPos];
-    surface^[destPos + 1] := imageBuffer[srcPos + 1];
-    surface^[destPos + 2] := imageBuffer[srcPos + 2];
-    surface^[destPos + 3] := imageBuffer[srcPos + 3];
-  end;
-end;
-
+uses Bitmap, VGA;
 
 exports
   { VGA }
