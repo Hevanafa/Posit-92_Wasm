380669bda2df59f71201a422327668f0f0249e97
Hevanafa <dhevansafa4@gmail.com>
2025-11-17 15:59:07 +0700

    moved image array into bitmap.pas

--- game.pas
+++ game.pas
@@ -4,17 +4,6 @@ library Game;
 
 uses Bitmap, VGA;
 
-var
-  images: array[1..10] of TBitmap;
-  nextImageHandle: longint = 1;
-
-function getImagePtr(const imgHandle: longint): pointer; public name 'getImagePtr';
-begin
-  if (1 <= imgHandle) and (imgHandle < nextImageHandle) then
-    getImagePtr := @images[imgHandle]
-  else
-    getImagePtr := nil;
-end;
 
 exports
   { VGA }
