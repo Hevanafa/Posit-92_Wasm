722c9bfb6681d800c1f3f45898eb3b4cfa99e24c
Hevanafa <dhevansafa4@gmail.com>
2025-11-17 13:22:58 +0700

    fixed VGA unit compile errors

--- game.pas
+++ game.pas
@@ -28,29 +28,34 @@ end;
 }
 
 { TODO: Use a PBitmap }
-
 procedure spr(const x, y, width, height: integer);
 var
   a, b, srcPos, destPos: integer;
+  surface: PByteArray;
 begin
+  surface := PByteArray(getSurface);
+
   for b:=0 to height - 1 do
   for a:=0 to width - 1 do begin
     srcPos := (b * width + a) * 4;
     destPos := ((y + b) * vgaWidth + (x + a)) * 4;
 
     { Order: RGBA }
-    surface[destPos] := imageBuffer[srcPos];
-    surface[destPos + 1] := imageBuffer[srcPos + 1];
-    surface[destPos + 2] := imageBuffer[srcPos + 2];
-    surface[destPos + 3] := imageBuffer[srcPos + 3];
+    surface^[destPos] := imageBuffer[srcPos];
+    surface^[destPos + 1] := imageBuffer[srcPos + 1];
+    surface^[destPos + 2] := imageBuffer[srcPos + 2];
+    surface^[destPos + 3] := imageBuffer[srcPos + 3];
   end;
 end;
 
 
 exports
+  { VGA }
   initBuffer,
   getSurface,
   cls,
+
+  { BITMAP }
   { allocImageData, }
   getImageBuffer,
   spr;
