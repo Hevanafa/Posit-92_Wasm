b28668e92a17719105ebe064b07340333ce28f76
Hevanafa <dhevansafa4@gmail.com>
2025-11-17 13:27:31 +0700

    added UNITS into compile script

--- compile.ps1
+++ compile.ps1
@@ -1,7 +1,10 @@
 # Compile targetting wasm32-embedded
-E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded .\game.pas
+E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded -FuUNITS .\game.pas
 
 if (test-path -path "game.wasm" -pathType leaf) {
-	del "game.wasm"
+  del "game.wasm"
 }
-ren "game" "game.wasm"
+
+if (test-path -path "game" -pathType leaf) {
+  ren "game" "game.wasm"
+}
