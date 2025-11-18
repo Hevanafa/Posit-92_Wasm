aecd3337d5b04f5a9f5fba9021f9bcbfc0c013b9
Hevanafa <dhevansafa4@gmail.com>
2025-11-19 04:41:29 +0700

    added variables in compile script

--- compile.ps1
+++ compile.ps1
@@ -1,10 +1,21 @@
-# Compile targetting wasm32-embedded
-E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded -FuUNITS .\game.pas
+$compilerPath = "E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe"
+$primaryUnit = ".\game.pas"
+$outputFile = "game"
 
-if (test-path -path "game.wasm" -pathType leaf) {
-  del "game.wasm"
+# Compile targetting wasm32-embedded
+E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded -FuUNITS $primaryUnit
+# This doesn't output to STDOUT
+# start-process -filePath $compilerPath -argumentList ("-Pwasm32", "-Tembedded", "-FuUNITS", $primaryUnit)
+
+if ($LastExitCode -ne 0) {
+  write-host "Compilation failed with exit code $LastExitCode" -foregroundColor red
+  exit $LastExitCode
 }
 
-if (test-path -path "game" -pathType leaf) {
-  ren "game" "game.wasm"
+if (test-path -path "$outputFile.wasm" -pathType leaf) {
+  del "$outputFile.wasm"
+}
+
+if (test-path -path "$outputFile" -pathType leaf) {
+  ren "$outputFile" "game.wasm"
 }
