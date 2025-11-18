41cdb7cbb4695ac3b2267040e248fdf835492241
Hevanafa <dhevansafa4@gmail.com>
2025-11-19 04:46:51 +0700

    expanded compile script with process start info

--- compile.ps1
+++ compile.ps1
@@ -3,9 +3,28 @@ $primaryUnit = ".\game.pas"
 $outputFile = "game"
 
 # Compile targetting wasm32-embedded
-E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded -FuUNITS $primaryUnit
+# E:\lazarus-wasm\fpc\bin\x86_64-win64\fpc.exe -Pwasm32 -Tembedded -FuUNITS $primaryUnit
+
 # This doesn't output to STDOUT
-# start-process -filePath $compilerPath -argumentList ("-Pwasm32", "-Tembedded", "-FuUNITS", $primaryUnit)
+$pinfo = new-object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $compilerPath
+$pinfo.Arguments = "-Pwasm32", "-Tembedded", "-FuUNITS", $primaryUnit
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+
+$p = new-object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$p.WaitForExit()
+
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+
+write-host "(STDOUT)"
+write-host $stdout
+write-host "(STDERR)"
+write-host $stderr
 
 if ($LastExitCode -ne 0) {
   write-host "Compilation failed with exit code $LastExitCode" -foregroundColor red
