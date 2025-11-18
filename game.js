fa6f8663973541ac4853643ff9630aa1fc7190c7
Hevanafa <dhevansafa4@gmail.com>
2025-11-18 14:42:38 +0700

    reordered example game code

--- game.js
+++ game.js
@@ -5,22 +5,22 @@ async function main() {
   const P92 = new Posit92("game");
   await P92.init();
 
-  // Begin render logic
-  P92.cls(0xFF6495ED);
-
   // Load assets
   const imgSatono = await P92.loadImage("assets/images/satono_diamond.png");
   const imgDefaultFont = await P92.loadImage("assets/fonts/nokia_cellphone_fc_8_0.png")
-  // const imgGasolineMaid = await P92.loadImage("assets/images/gasoline_maid.png")
+  const imgGasolineMaid = await P92.loadImage("assets/images/gasoline_maid.png")
+
+  // Begin render logic
+  P92.cls(0xFF6495ED);
 
   console.log("imgSatono handle:", imgSatono);
   console.log("imgDefaultFont handle:", imgDefaultFont);
 
-  P92.debugImage(imgSatono);
-  P92.debugImage(imgDefaultFont);
   // P92.debugImage(imgGasolineMaid);
+  // P92.debugImage(imgSatono);
+  // P92.debugImage(imgDefaultFont);
   
-  // P92.spr(imgGasolineMaid, 0, 0);
+  P92.spr(imgGasolineMaid, 0, 0);
 
   P92.spr(imgSatono, 50, 10);
   P92.sprRegion(imgSatono, 0, 0, 10, 10, 30, 10);
