1420beb4789d79754fce247676663f582afcfb4e
Hevanafa <dhevansafa4@gmail.com>
2025-11-18 12:27:18 +0700

    added default font definition

--- game.pas
+++ game.pas
@@ -4,6 +4,19 @@ library Game;
 
 uses Bitmap, BMFont, Graphics, Logger, VGA;
 
+var
+  _defaultFont: TBMFont;
+  _defaultFontGlyphs: array[32..126] of TBMFontGlyph;
+
+function defaultFont: pointer;
+begin
+  defaultFont := @_defaultFont
+end;
+
+function defaultFontGlyphs: pointer;
+begin
+  defaultFontGlyphs := @_defaultFontGlyphs
+end;
 
 exports
   { VGA }
