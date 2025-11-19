6f9565e8d0f80a96a7baab5792e66114420771b3
Hevanafa <dhevansafa4@gmail.com>
2025-11-19 14:48:06 +0700

    decluttered starting point

--- game.js
+++ game.js
@@ -1,26 +1,9 @@
-const white = 0xFFFFFFFF;
-const yellow = 0xFFFFFF55;
-
 var done = false;
 
 async function main() {
   const P92 = new Posit92("game");
   await P92.init();
-
   await P92.loadAssets();
-
-  // Draw only 1 frame
-
-  // P92.update();
-  // P92.draw();
-
-  // P92.stressTest();
-
-  // Stress test
-  // while (!done) {
-  //   P92.update();
-  //   P92.draw();
-  // }
 
   function loop() {
     if (done) return;
@@ -28,19 +11,8 @@ async function main() {
     P92.update();
     P92.draw();
     requestAnimationFrame(loop)
-
-    // P92.printDefault("Hello from POSIT-92!", 10, 10);
-    // P92.spr(imgGasolineMaid, 10, 30);
-    // P92.flush();
   }
   loop();
-
-
-  // Stress test 1
-  // P92.startStressTest();
-
-  // Stress test 2
-  // P92.startBenchmark();
 }
 
 main()
