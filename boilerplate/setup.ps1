c099cab0169541ca4b3d2b2a43bde0bd581e16f5
Hevanafa <dhevansafa4@gmail.com>
2025-11-21 07:38:10 +0700

    added about text in boilerplate setup

--- boilerplate/setup.ps1
+++ boilerplate/setup.ps1
@@ -1,3 +1,11 @@
-copy-item "..\UNITS\*.pas" ".\"
-copy-item "..\*.ps1" ".\"
-copy-item "..\posit-92.js" ".\"
+# Setup Boilerplate
+# By Hevanafa, 21-11-2025
+# Part of Posit-92 framework
+
+# This script should be executed before copying as a new demo
+
+$source = ".."
+
+copy-item "$source\UNITS\*.pas" ".\"
+copy-item "$source\*.ps1" ".\"
+copy-item "$source\posit-92.js" ".\"
