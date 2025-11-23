ef6260c741e8285f3706df59a6e423544d2e05fe
Hevanafa <dhevansafa4@gmail.com>
2025-11-23 20:11:29 +0700

    added copy item lines in dist script

--- boilerplate/dist.ps1
+++ boilerplate/dist.ps1
@@ -1,8 +1,20 @@
-$wasmFile = "wasm"
+$wasmFile = "game.wasm"
+$distDir = "dist"
 
-if (test-path -path "$wasmFile.wasm" -pathType leaf) {}
+if (test-path -path $wasmFile -pathType leaf) {}
 else {
-  write-host "Missing $wasmFile.wasm!" -foregroundColor red
+  write-host "Missing $wasmFile!" -foregroundColor red
   exit 1
 }
 
+if (test-path -path $distDir -pathType container) {}
+else {
+  mkdir $distDir
+}
+
+copy-item "game.js" "$distDir\"
+copy-item $wasmFile "$distDir\"
+copy-item "posit-92.js" "$distDir\"
+copy-item "index.html" "$distDir\"
+
+# TODO: Copy assets folder recursively to $distDir
