3185234bd464b7a1df5f8daa9b1bb11b0d021ce2
Hevanafa <dhevansafa4@gmail.com>
2025-11-24 15:58:08 +0700

    copied prompt_demo from immediate_gui

---
+++ DEMOS/prompt_demo/game.pas
@@ -0,0 +1,178 @@
+{
+  Immediate GUI Implementation
+  Part of Posit-92 framework
+  By Hevanafa, 22-11-2025
+
+  Based on my QB64 Immediate GUI implementation
+}
+
+library Game;
+
+{$Mode ObjFPC}
+
+uses Bitmap, BMFont, Conv, FPS,
+  Graphics, ImmedGui, Keyboard, Logger,
+  Mouse, Panic, Shapes, Sounds,
+  Timing, VGA,
+  Assets;
+
+const
+  SC_ESC = $01;
+  SC_SPACE = $39;
+
+
+var
+  lastEsc: boolean;
+
+  { Init your game state here }
+  gameTime: double;
+  clicks: word;
+  showFPS: TCheckboxState;
+  listItems: array[0..2] of string;
+  sliderValue: TSliderState;
+
+{ Use this to set `done` to true }
+procedure signalDone; external 'env' name 'signalDone';
+
+procedure drawFPS;
+begin
+  printDefault('FPS:' + i32str(getLastFPS), 240, 0);
+end;
+
+procedure drawMouse;
+begin
+  if hasHoveredWidget then
+    spr(imgHandCursor, mouseX - 5, mouseY - 1)
+  else
+    spr(imgCursor, mouseX, mouseY);
+end;
+
+procedure replaceColours(const imgHandle: longint; const oldColour, newColour: longword);
+var
+  a, b: word;
+  image: PBitmap;
+begin
+  if not isImageSet(imgHandle) then begin
+    writeLog('replaceColours: Unset imgHandle: ' + i32str(imgHandle));
+    exit
+  end;
+
+  image := getImagePtr(imgHandle);
+
+  for b:=0 to image^.height - 1 do
+  for a:=0 to image^.width - 1 do
+    if unsafeSprPget(image, a, b) = oldColour then
+      unsafeSprPset(image, a, b, newColour);
+end;
+
+
+procedure init;
+begin
+  initBuffer;
+  initDeltaTime;
+  initFPSCounter;
+end;
+
+procedure afterInit;
+var
+  a: integer;
+begin
+  { Initialise game state here }
+  hideCursor;
+
+  initImmediateGUI;
+  guiSetFont(defaultFont, defaultFontGlyphs);
+
+  replaceColours(blackFont.imgHandle, $FFFFFFFF, $FF000000);
+
+  clicks := 0;
+
+  for a:=0 to high(listItems) do
+    listItems[a] := 'ListItem' + i32str(a);
+end;
+
+procedure update;
+begin
+  updateDeltaTime;
+  incrementFPS;
+
+  updateGUILastMouseButton;
+  updateMouse;
+  updateGUIMouseZone;
+
+  { Your update logic here }
+  if lastEsc <> isKeyDown(SC_ESC) then begin
+    lastEsc := isKeyDown(SC_ESC);
+
+    if lastEsc then begin
+      writeLog('ESC is pressed!');
+      signalDone
+    end;
+  end;
+
+  gameTime := gameTime + dt;
+
+  resetWidgetIndices
+end;
+
+procedure draw;
+var
+  w: integer;
+  s: string;
+begin
+  cls($FF6495ED);
+
+  guiSetFont(blackFont, blackFontGlyphs);
+  if Button('Click me!', 180, 88, 50, 24) then
+    inc(clicks);
+
+  if ImageButton(240, 88, imgWinNormal, imgWinHovered, imgWinPressed) then
+    inc(clicks);
+
+  if (trunc(gameTime * 4) and 1) > 0 then
+    spr(imgDosuEXE[1], 148, 88)
+  else
+    spr(imgDosuEXE[0], 148, 88);
+
+  { spr(imgDosuEXE[0], 100, 80); }
+  sprStretch(imgDosuEXE[0], 100, 80, 24, 48);
+
+  guiSetFont(defaultFont, defaultFontGlyphs);
+  Slider(120, 40, 100, sliderValue, 0, 100);
+  TextLabel('Slider value: ' + i32str(sliderValue.value), 120, 30);
+
+  s := 'Clicks: ' + i32str(clicks);
+  w := guiMeasureText(s);
+  TextLabel(s, (vgaWidth - w) div 2, 120);
+
+  guiSetFont(picotronFont, picotronFontGlyphs);
+  s := 'Picotron font';
+  w := guiMeasureText(s);
+  TextLabel(s, (vgaWidth - w) div 2, 140);
+
+  guiSetFont(defaultFont, defaultFontGlyphs);
+  ProgressBar(10, 80, 80, 10, 0.75);
+  ProgressBarLabelled(10, 100, 80, 10, 0.75);
+  Checkbox('Show FPS', 10, 60, showFPS);
+  ListView(10, 10, listItems, 2);
+
+  resetActiveWidget;
+
+  drawMouse;
+
+  if showFPS.checked then drawFPS;
+
+  flush
+end;
+
+exports
+  { Main game procedures }
+  init,
+  afterInit,
+  update,
+  draw;
+
+begin
+{ Starting point is intentionally left empty }
+end.
+
---
+++ DEMOS/prompt_demo/posit-92.js
@@ -0,0 +1,600 @@
+"use strict";
+
+/**
+ * Game assets are loaded in loadAssets()
+ */
+
+/**
+ * KeyboardEvent.code to DOS scancode
+ */
+const ScancodeMap = {
+  "Escape": 0x01,
+  "Space": 0x39
+  // Add more scancodes as necessary
+};
+
+class Posit92 {
+  #displayScale = Object.freeze(2);
+  #wasmSource = "game.wasm";
+
+  #vgaWidth = 320;
+  #vgaHeight = 200;
+
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  #canvas;
+  #ctx;
+
+  /**
+   * @type {WebAssembly.Instance}
+   */
+  #wasm;
+
+  /**
+   * @type {AudioContext}
+   */
+  #audioContext;
+
+  /**
+   * @type {Map<number, AudioBuffer>}
+   */
+  #sounds = new Map();
+  #soundVolumes = new Map();
+
+  /**
+   * @type {AudioBufferSourceNode | null}
+   */
+  #music = null;
+  #musicVolume = 1.0;
+  #musicGainNode = null;
+
+  /**
+   * For use with WebAssembly init
+   */
+  #importObject = Object.freeze({
+    env: {
+      _haltproc: exitcode => console.log("Programme halted with code:", exitcode),
+
+      hideCursor: () => this.hideCursor(),
+      showCursor: () => this.showCursor(),
+
+      // Keyboard
+      isKeyDown: scancode => this.isKeyDown(scancode),
+      signalDone: () => { done = true },
+
+      // Logger
+      writeLogI32: value => console.log("Pascal (i32):", value),
+      flushLog: () => this.pascalWriteLog(),
+
+      // Mouse
+      getMouseX: () => this.getMouseX(),
+      getMouseY: () => this.getMouseY(),
+      getMouseButton: () => this.getMouseButton(),
+
+      // Panic
+      panicHalt: this.panicHalt.bind(this),
+
+      // Sounds
+      playSound: this.playSound.bind(this),
+      playMusic: this.playMusic.bind(this),
+      setSoundVolume: this.setSoundVolume.bind(this),
+      setMusicVolume: this.setMusicVolume.bind(this),
+      stopMusic: () => this.stopMusic(),
+
+      // Timing
+      getTimer: () => this.getTimer(),
+
+      // VGA
+      flush: () => this.flush()
+    }
+  });
+
+  constructor(canvasID) {
+    if (canvasID == null)
+      throw new Error("canvasID is required!");
+
+    this.#assertString(canvasID);
+
+    this.#canvas = document.getElementById(canvasID);
+    if (this.#canvas == null)
+      throw new Error(`Couldn't find canvasID \"${ canvasID }\"`);
+
+    this.#ctx = this.#canvas.getContext("2d");
+  }
+
+  // Init segment
+  async #initWebAssembly() {
+    const response = await fetch(this.#wasmSource);
+    const bytes = await response.arrayBuffer();
+    const result = await WebAssembly.instantiate(bytes, this.#importObject);
+    this.#wasm = result.instance;
+  }
+
+  #initAudio() {
+    this.#audioContext = new AudioContext();
+  }
+
+  async init() {
+    await this.#initWebAssembly();
+    this.#wasm.exports.init();
+    this.#initKeyboard();
+    this.#initMouse();
+    this.#initAudio();
+  }
+
+  afterInit() {
+    this.#wasm.exports.afterInit();
+    this.#addOutOfFocusFix()
+  }
+
+  #addOutOfFocusFix() {
+    this.#canvas.addEventListener("click", () => {
+      this.#canvas.tabIndex = 0;
+      this.#canvas.focus()
+    })
+  }
+
+  async loadAssets() {
+    let handle = 0;
+
+    handle = await this.loadImage("assets/images/cursor.png");
+    this.#wasm.exports.setImgCursor(handle);
+    handle = await this.loadImage("assets/images/hand.png");
+    this.#wasm.exports.setImgHandCursor(handle);
+
+    await this.loadBMFont(
+      "assets/fonts/nokia_cellphone_fc_8.txt",
+      this.#wasm.exports.defaultFontPtr(),
+      this.#wasm.exports.defaultFontGlyphsPtr());
+    await this.loadBMFont(
+      "assets/fonts/nokia_cellphone_fc_8.txt",
+      this.#wasm.exports.blackFontPtr(),
+      this.#wasm.exports.blackFontGlyphsPtr());
+
+    await this.loadBMFont(
+      "assets/fonts/picotron_8px.txt",
+      this.#wasm.exports.picotronFontPtr(),
+      this.#wasm.exports.picotronFontGlyphsPtr());
+
+    handle = await this.loadImage("assets/images/dosu_1.png");
+    this.#wasm.exports.setImgDosuEXE(handle, 0);
+    handle = await this.loadImage("assets/images/dosu_2.png");
+    this.#wasm.exports.setImgDosuEXE(handle, 1);
+
+    this.#wasm.exports.setImgWinNormal(
+      await this.loadImage("assets/images/btn_normal.png"));
+
+    this.#wasm.exports.setImgWinHovered(
+      await this.loadImage("assets/images/btn_hovered.png"));
+
+    this.#wasm.exports.setImgWinPressed(
+      await this.loadImage("assets/images/btn_pressed.png"));
+
+
+    // Add more assets as necessary
+  }
+
+  cleanup() {
+    this.stopMusic();
+    this.showCursor();
+  }
+
+  hideCursor() {
+    this.#canvas.style.cursor = "none"
+  }
+
+  showCursor() {
+    this.#canvas.style.removeProperty("cursor")
+  }
+
+  #assertNumber(value) {
+    if (value == null)
+      throw new Error("Expected a number, but received null");
+
+    if (typeof value != "number")
+      throw new Error(`Expected a number, but received ${typeof value}`);
+
+    if (isNaN(value))
+      throw new Error("Expected a number, but received NaN");
+  }
+
+  #assertString(value) {
+    if (typeof value != "string")
+      throw new Error(`Expected a string, but received ${typeof value}`);
+  }
+
+
+  // BITMAP.PAS
+  async loadImageFromURL(url) {
+    if (url == null)
+      throw new Error("loadImageFromURL: url is required");
+
+    this.#assertString(url);
+
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+      img.src = url
+    })
+  }
+
+  debugImage(imgHandle) {
+    this.#wasm.exports.debugImage(imgHandle)
+  }
+
+  async loadImage(url) {
+    if (url == null)
+      throw new Error("loadImage: url is required");
+
+    this.#assertString(url);
+
+    const img = await this.loadImageFromURL(url);
+
+    // Copy image
+    const tempCanvas = document.createElement("canvas");
+    tempCanvas.width = img.width;
+    tempCanvas.height = img.height;
+    const tempCtx = tempCanvas.getContext("2d");
+    tempCtx.drawImage(img, 0, 0);
+    const pixels = tempCtx.getImageData(0, 0, img.width, img.height).data;
+
+    // Obtain a new handle number
+    const imgHandle = this.#wasm.exports.loadImageHandle();
+    const bitmapPtr = this.#wasm.exports.getImagePtr(imgHandle);
+    
+    // Write to TBitmap
+    const memory = new Uint8Array(this.#wasm.exports.memory.buffer, bitmapPtr);
+    memory[0] = img.width & 0xff;
+    memory[1] = (img.width >> 8) & 0xff;
+    memory[2] = img.height & 0xff;
+    memory[3] = (img.height >> 8) & 0xff;
+    memory.set(pixels, 4);  // TBitmap.data
+
+    return imgHandle
+  }
+
+
+  // BMFONT.PAS
+  #newBMFontGlyph() {
+    return {
+      id: 0,
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+      xoffset: 0,
+      yoffset: 0,
+      xadvance: 0,
+      lineHeight: 0
+    }
+  }
+
+  async loadBMFont(url, fontPtrRef, fontGlyphsPtrRef) {
+    if (url == null)
+      throw new Error("loadBMFont: url is required");
+
+    this.#assertString(url);
+    this.#assertNumber(fontPtrRef);
+    this.#assertNumber(fontGlyphsPtrRef);
+
+    const res = await fetch(url);
+    const text = await res.text();
+
+    const lines = text.endsWith("\r\n") ? text.split("\r\n") : text.split("\n");
+
+    let txtLine = "";
+    /**
+     * @type {Array<[string, string]>}
+     */
+    let pairs;
+    let k = "", v = "";
+
+    let lineHeight = 0;
+    // font bitmap URL
+    let filename = "";
+    const fontGlyphs = {};
+    let glyphCount = 0;
+    let imgHandle = 0;
+
+    for (const line of lines) {
+      txtLine = line.replaceAll(/\s+/g, " ");
+      
+      pairs = txtLine.split(" ").map(part => part.split("="));
+
+      if (txtLine.startsWith("info")) {
+        [k, v] = pairs.find(pair => pair[0] == "face");
+
+      } else if (txtLine.startsWith("common")) {
+        [k, v] = pairs.find(pair => pair[0] == "lineHeight");
+        lineHeight = parseInt(v);
+
+      } else if (txtLine.startsWith("page")) {
+        [k, v] = pairs.find(pair => pair[0] == "file");
+        filename = v.replaceAll(/"/g, "");
+
+      } else if (txtLine.startsWith("char") && !txtLine.startsWith("chars")) {
+        const tempGlyph = this.#newBMFontGlyph();
+
+        for (const [k, v] of pairs) {
+          switch (k) {
+            case "id": tempGlyph.id = parseInt(v); break;
+            case "x": tempGlyph.x = parseInt(v); break;
+            case "y": tempGlyph.y = parseInt(v); break;
+            case "width": tempGlyph.width = parseInt(v); break;
+            case "height": tempGlyph.height = parseInt(v); break;
+            case "xoffset": tempGlyph.xoffset = parseInt(v); break;
+            case "yoffset": tempGlyph.yoffset = parseInt(v); break;
+            case "xadvance": tempGlyph.xadvance = parseInt(v); break;
+          }
+        }
+
+        fontGlyphs[tempGlyph.id] = tempGlyph;
+        glyphCount++
+      }
+    }
+
+    console.log("Loaded", glyphCount, "glyphs");
+
+    // Load font bitmap
+    imgHandle = await this.loadImage(filename);
+    // console.log("loadBMFont imgHandle:", imgHandle);
+
+    const fontPtr = fontPtrRef;
+    const glyphsPtr = fontGlyphsPtrRef;
+
+    // Write font data
+    const fontMem = new DataView(this.#wasm.exports.memory.buffer, fontPtr);
+
+    let offset = 0;
+    // Skip face string
+    offset += 16;  // was 256
+    // Skip filename string
+    offset += 64;  // was 256
+
+    // true makes it little-endian
+    fontMem.setUint16(offset, lineHeight, true);
+    
+    // +2 requires a packed record because Pascal records are padded by default
+    fontMem.setInt32(offset + 2, imgHandle, true);
+
+    // Write glyphs
+    const glyphsMem = new DataView(this.#wasm.exports.memory.buffer, glyphsPtr);
+
+    for (const charID in fontGlyphs) {
+      const glyph = fontGlyphs[charID];
+      const id = parseInt(charID);
+
+      // Range check
+      if (id < 32 || id > 126) continue;
+
+      // 16 is from the 8 fields of TBMFontGlyph, all 2 bytes
+      const glyphOffset = (id - 32) * 16;
+
+      glyphsMem.setUint16(glyphOffset + 0, glyph.id, true);
+
+      glyphsMem.setUint16(glyphOffset + 2, glyph.x, true);
+      glyphsMem.setUint16(glyphOffset + 4, glyph.y, true);
+      glyphsMem.setUint16(glyphOffset + 6, glyph.width, true);
+      glyphsMem.setUint16(glyphOffset + 8, glyph.height, true);
+
+      glyphsMem.setInt16(glyphOffset + 10, glyph.xoffset, true);
+      glyphsMem.setInt16(glyphOffset + 12, glyph.yoffset, true);
+      glyphsMem.setInt16(glyphOffset + 14, glyph.xadvance, true);
+    }
+
+    console.log("loadBMFont completed");
+  }
+
+
+  // KEYBOARD.PAS
+  heldScancodes = new Set();
+
+  #initKeyboard() {
+    window.addEventListener("keydown", e => {
+      // console.log("keydown", e.code);
+
+      const scancode = ScancodeMap[e.code];
+      if (scancode) {
+        this.heldScancodes.add(scancode);
+        e.preventDefault();
+      }
+    })
+
+    window.addEventListener("keyup", e => {
+      const scancode = ScancodeMap[e.code];
+      if (scancode) this.heldScancodes.delete(scancode)
+    })
+  }
+
+  isKeyDown(scancode) {
+    return this.heldScancodes.has(scancode)
+  }
+
+
+  // MOUSE.PAS
+  #mouseX = 0;
+  #mouseY = 0;
+  #mouseButton = 0;
+
+  #leftButtonDown = false;
+  #rightButtonDown = false;
+
+  #initMouse() {
+    this.#canvas.addEventListener("mousemove", e => {
+      const rect = this.#canvas.getBoundingClientRect();
+      this.#mouseX = Math.floor((e.clientX - rect.left) / this.#displayScale);
+      this.#mouseY = Math.floor((e.clientY - rect.top) / this.#displayScale);
+    });
+
+    this.#canvas.addEventListener("mousedown", e => {
+      if (e.button == 0) this.#leftButtonDown = true;
+      if (e.button == 2) this.#rightButtonDown = true;
+      this.#updateMouseButton();
+      e.preventDefault();  // Prevent context menu on right click
+    });
+
+    this.#canvas.addEventListener("mouseup", e => {
+      if (e.button == 0) this.#leftButtonDown = false;
+      if (e.button == 2) this.#rightButtonDown = false;
+      this.#updateMouseButton();
+    });
+
+    this.#canvas.addEventListener("contextmenu", e => {
+      e.preventDefault()
+    });
+  }
+
+  getMouseX() { return this.#mouseX }
+  getMouseY() { return this.#mouseY }
+  getMouseButton() { return this.#mouseButton }
+
+  #updateMouseButton() {
+    if (this.#leftButtonDown && this.#rightButtonDown)
+      this.#mouseButton = 3
+    else if (this.#rightButtonDown)
+      this.#mouseButton = 2
+    else if (this.#leftButtonDown)
+      this.#mouseButton = 1
+    else
+      this.#mouseButton = 0;
+  }
+
+
+  // LOGGER.PAS
+  pascalWriteLog() {
+    const bufferPtr = this.#wasm.exports.getLogBuffer();
+    const buffer = new Uint8Array(this.#wasm.exports.memory.buffer, bufferPtr, 256);
+
+    const len = buffer[0];
+    const msgBytes = buffer.slice(1, 1 + len);
+    const msg = new TextDecoder().decode(msgBytes);
+
+    console.log("Pascal:", msg);
+  }
+
+
+  // PANIC.PAS
+  panicHalt(textPtr, textLen) {
+    const buffer = new Uint8Array(this.#wasm.exports.memory.buffer, textPtr, textLen);
+    const msg = new TextDecoder().decode(buffer);
+
+    done = true;
+    throw new Error(`PANIC: ${msg}`)
+  }
+
+
+  // SOUNDS.PAS
+  async loadSound(key, url) {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await this.#audioContext.decodeAudioData(arrayBuffer);
+
+    this.#sounds.set(key, audioBuffer);
+    this.setSoundVolume(key, 0.5)
+  }
+
+  playSound(key) {
+    const buffer = this.#sounds.get(key);
+    if (buffer == null) {
+      console.warn("Sound " + key + " is not loaded");
+      return
+    }
+
+    const volume = this.#soundVolumes.get(key);
+
+    const source = this.#audioContext.createBufferSource();
+    const gainNode = this.#audioContext.createGain();
+
+    source.buffer = buffer;
+    gainNode.gain.value = volume;
+
+    // Connect source -> gain -> destination
+    source.connect(gainNode);
+    gainNode.connect(this.#audioContext.destination);
+    source.start(0)
+    // source automatically disconnects when done
+  }
+
+  playMusic(key) {
+    this.stopMusic();
+
+    const buffer = this.#sounds.get(key);
+    if (buffer == null) {
+      console.warn("Music " + key + " is not loaded");
+      return
+    }
+
+    this.#music = this.#audioContext.createBufferSource();
+    this.#musicGainNode = this.#audioContext.createGain();
+
+    this.#music.buffer = buffer;
+    this.#music.loop = true;
+    this.#musicGainNode.gain.value = this.#musicVolume;
+
+    // Connect source -> gain -> destination
+    this.#music.connect(this.#musicGainNode);
+    this.#musicGainNode.connect(this.#audioContext.destination);
+    this.#music.start(0)
+  }
+
+  #clamp(value, min, max) {
+    this.#assertNumber(value);
+    this.#assertNumber(min);
+    this.#assertNumber(max);
+
+    return Math.max(min, Math.min(max, value))
+  }
+
+  setSoundVolume(key, volume) {
+    const clamped = this.#clamp(volume, 0.0, 1.0);
+    this.#soundVolumes.set(key, clamped)
+  }
+
+  setMusicVolume(volume) {
+    this.#musicVolume = this.#clamp(volume, 0.0, 1.0);
+
+    if (this.#musicGainNode != null)
+      this.#musicGainNode.gain.value = this.#musicVolume;
+  }
+
+  stopMusic() {
+    if (this.#music == null) return;
+
+    this.#music.stop();
+    this.#music = null;
+    this.#musicGainNode = null
+  }
+
+
+  // TIMING.PAS
+  getTimer() {
+    return Date.now() / 1000
+  }
+
+
+  // VGA.PAS
+  flush() {
+    const surfacePtr = this.#wasm.exports.getSurface();
+    const imageData = new Uint8ClampedArray(
+      this.#wasm.exports.memory.buffer,
+      surfacePtr,
+      this.#vgaWidth * this.#vgaHeight * 4
+    );
+
+    const imgData = new ImageData(imageData, this.#vgaWidth, this.#vgaHeight);
+
+    this.#ctx.putImageData(imgData, 0, 0);
+  }
+
+
+  // Game loop
+  update() {
+    this.#wasm.exports.update()
+  }
+
+  draw() {
+    this.#wasm.exports.draw()
+  }
+}
