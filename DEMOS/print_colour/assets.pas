0b7cf3ec0037248cdbf8423acae8a79d3cec24f2
Hevanafa <dhevansafa4@gmail.com>
2025-11-27 21:12:50 +0700

    moved printCharColour to BMFont

--- DEMOS/print_colour/assets.pas
+++ DEMOS/print_colour/assets.pas
@@ -21,20 +21,6 @@ function defaultFontGlyphsPtr: PBMFontGlyph; public name 'defaultFontGlyphsPtr';
 procedure printDefault(const text: string; const x, y: integer);
 function measureDefault(const text: string): word;
 
-function printCharColour(
-  const ch: char;
-  const x, y: integer;
-  const font: TBMFont;
-  const fontGlyphs: array of TBMFontGlyph;
-  const colour: longword): word;
-
-procedure printColour(
-  const text: string;
-  const x, y: integer;
-  const font: TBMFont;
-  const fontGlyphs: array of TBMFontGlyph;
-  const colour: longword);
-
 
 { Asset boilerplate }
 procedure setImgCursor(const imgHandle: longint); public name 'setImgCursor';
@@ -65,57 +51,6 @@ end;
 function measureDefault(const text: string): word;
 begin
   measureDefault := measureBMFont(text, defaultFontGlyphs)
-end;
-
-
-{ Returns xadvance for the next char }
-function printCharColour(
-  const ch: char;
-  const x, y: integer;
-  const font: TBMFont;
-  const fontGlyphs: array of TBMFontGlyph;
-  const colour: longword): word;
-var
-  charcode: byte;
-  glyphIdx: integer;
-  glyph: TBMFontGlyph;
-  result: word;
-begin
-  charcode := ord(ch);
-  glyphIdx := charcode - 32;
-
-  result := 0;
-
-  if (glyphIdx in [low(fontGlyphs)..high(fontGlyphs)]) then begin
-    glyph := fontGlyphs[glyphIdx];
-
-    sprRegionSolid(
-      font.imgHandle,
-      glyph.x, glyph.y,
-      glyph.width, glyph.height,
-      x + glyph.xoffset, y + glyph.yoffset,
-      colour);
-
-    result := glyph.xadvance
-  end;
-
-  printCharColour := result
-end;
-
-procedure printColour(
-  const text: string;
-  const x, y: integer;
-  const font: TBMFont;
-  const fontGlyphs: array of TBMFontGlyph;
-  const colour: longword);
-var
-  a: word;
-  left: integer;
-begin
-  left := 0;
-
-  for a:=1 to length(text) do
-    inc(left, printCharColour(text[a], x + left, y, font, fontGlyphs, colour))
 end;
 
 
