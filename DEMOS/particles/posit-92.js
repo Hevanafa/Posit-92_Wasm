f625664e4b41d45ef33119ba0f1c183b54d5228b
Hevanafa <dhevansafa4@gmail.com>
2025-11-28 15:19:58 +0700

    added Wasm unit prototype

--- DEMOS/particles/posit-92.js
+++ DEMOS/particles/posit-92.js
@@ -60,6 +60,8 @@ class Posit92 {
       hideCursor: () => this.hideCursor(),
       showCursor: () => this.showCursor(),
 
+      wasmgetmem: bytes => this.#WasmGetMem(bytes),
+
       // Keyboard
       isKeyDown: scancode => this.isKeyDown(scancode),
       signalDone: () => { done = true },
@@ -193,10 +195,6 @@ class Posit92 {
     })
   }
 
-  debugImage(imgHandle) {
-    this.#wasm.exports.debugImage(imgHandle)
-  }
-
   // Used in loadImageRef
   #images = [];
 
@@ -219,7 +217,7 @@ class Posit92 {
 
     const wasmMemory = new Uint8Array(this.#wasm.exports.memory.buffer);
     const byteSize = img.width * img.height * 4;
-    const wasmPtr = this.#wasmGetmem(byteSize);
+    const wasmPtr = this.#WasmGetMem(byteSize);
     wasmMemory.set(imageData.data, wasmPtr)
 
     if (this.#images.length == 0)
@@ -236,7 +234,7 @@ class Posit92 {
   // Start at 1 MB
   #wasmMemoryOffset = 1048576;
 
-  #wasmGetmem(bytes) {
+  #WasmGetMem(bytes) {
     const ptr = this.#wasmMemoryOffset;
     this.#wasmMemoryOffset += bytes;
 
