da527a366f7f320f2b28467780685c0e756c5088
Hevanafa <dhevansafa4@gmail.com>
2025-11-29 17:43:17 +0700

    updated main Sounds unit

--- snippets/music_loop.pas
+++ snippets/music_loop.pas
@@ -1,14 +1,12 @@
 { Step 1: Enable music repeat on afterInit }
-setMusicRepeat(true)
+setMusicRepeat(true);
 
 
 { Step 2: Add this at the end of update routine }
 
 { Handle music repeat manually (only when it is playing)
   Important: #musicPlayer.loop must be turned off! }
-if getMusicPlaying and (getMusicTime >= getMusicDuration - 0.05) then
-  if getMusicRepeat then begin
-    stopMusic;
-    playMusic({ Your BGM key })
-  end else
-    stopMusic;
+handleMusicRepeat({ Your BGM key });
+
+{ Alt version with a state variable: }
+handleMusicRepeat(actualMusicKey);
