d38fbeee5ad823858b3acb384c38914ea632da08
Hevanafa <dhevansafa4@gmail.com>
2025-11-30 13:22:42 +0700

    Moved WebGL methods from Posit92

--- DEMOS/webgl_demo/posit-92.js
+++ DEMOS/webgl_demo/posit-92.js
@@ -24,10 +24,6 @@ class Posit92 {
    */
   #canvas;
   // #ctx;
-  /**
-   * @type {WebGLRenderingContext}
-   */
-  #gl;
 
   /**
    * @type {WebAssembly.Instance}
@@ -167,6 +163,10 @@ class Posit92 {
     }
   });
 
+  getWasmImportObject() {
+    return this.#importObject
+  }
+
   #handleHaltProc(exitcode) {
     console.log("Programme halted with code:", exitcode);
     this.cleanup();
@@ -189,10 +189,6 @@ class Posit92 {
       throw new Error(`Couldn't find canvasID \"${ canvasID }\"`);
 
     // this.#ctx = this.#canvas.getContext("2d");
-    this.#gl = this.#canvas.getContext("webgl") ?? this.#canvas.getContext("experimental-webgl");
-
-    if (this.#gl == null)
-      throw new Error("WebGL is not supported!");
   }
 
   // Init segment
@@ -775,6 +771,8 @@ class Posit92 {
 
   // VGA.PAS
   flush() {
+    // throw new Error("Attempting to call flush in WebGL context");
+
     const surfacePtr = this.#wasm.exports.getSurfacePtr();
     const imageData = new Uint8ClampedArray(
       this.#wasm.exports.memory.buffer,
@@ -784,9 +782,7 @@ class Posit92 {
 
     const imgData = new ImageData(imageData, this.#vgaWidth, this.#vgaHeight);
 
-    throw new Error("Attempting to call flush in WebGL");
-
-    // this.#ctx.putImageData(imgData, 0, 0);
+    this.#ctx.putImageData(imgData, 0, 0);
   }
 
   toggleFullscreen() {
@@ -795,179 +791,6 @@ class Posit92 {
     else
       document.exitFullscreen();
   }
-
-  // WEBGL.PAS
-  #readCString(ptr) {
-    this.#assertNumber(ptr);
-
-    const memory = new Uint8Array(this.#wasm.exports.memory.buffer);
-    let end = ptr;
-    while (memory[end] != 0) end++;  // Find null terminator
-
-    const bytes = memory.subarray(ptr, end);
-    return new TextDecoder().decode(bytes)
-  }
-
-  /**
-   * @type {Map<number, WebGLTexture>}
-   */
-  #textures = new Map();
-  #nextTextureId = 1;
-
-  #glCreateTexture() {
-    const texture = this.#gl.createTexture();
-    const id = this.#nextTextureId++;
-    console.log("id", id, this.#nextTextureId);
-    this.#textures.set(id, texture);
-    return 1
-  }
-
-  #glBindTexture(target, textureId) {
-    const texture = this.#textures.get(textureId);
-    this.#gl.bindTexture(target, texture)
-  }
-
-  #glTextParameteri(target, pname, param) {
-    this.#gl.texParameteri(target, pname, param)
-  }
-
-  #glTexImage2D(
-    target, level, internalFormat,
-    width, height, border,
-    format, type, pixelsPtr) {
-
-    const pixels = new Uint8Array(
-      this.#wasm.exports.memory.buffer,
-      pixelsPtr,
-      width * height * 4);
-
-    this.#gl.texImage2D(
-      target, level, internalFormat,
-      width, height, border,
-      format, type, pixels)
-  }
-
-  #shaders = new Map();
-  #nextShaderId = 1;
-  #programs = new Map();
-  #nextProgramId = 1;
-
-  #glCreateShader(type) {
-    const shader = this.#gl.createShader(type);
-    const id = this.#nextShaderId++;
-    this.#shaders.set(id, shader);
-    return id
-  }
-
-  #glShaderSource(shaderId, sourcePtr) {
-    const shader = this.#shaders.get(shaderId);
-    const source = this.#readCString(sourcePtr);
-    this.#gl.shaderSource(shader, source)
-  }
-
-  #glCompileShader(shaderId) {
-    const shader = this.#shaders.get(shaderId);
-    this.#gl.compileShader(shader)
-  }
-
-  #glCreateProgram() {
-    const program = this.#gl.createProgram();
-    const id = this.#nextProgramId++;
-    this.#programs.set(id, program);
-    return id
-  }
-
-  #glAttachShader(programId, shaderId) {
-    const program = this.#programs.get(programId);
-    const shader = this.#shaders.get(shaderId);
-    this.#gl.attachShader(program, shader)
-  }
-
-  #glLinkProgram(programId) {
-    const program = this.#programs.get(programId);
-    this.#gl.linkProgram(program)
-  }
-
-  #glUseProgram(programId) {
-    const program = this.#programs.get(programId);
-    this.#gl.useProgram(program)
-  }
-
-  #buffers = new Map();
-  #nextBufferId = 1;
-
-  #glCreateBuffer() {
-    const buffer = this.#gl.createBuffer();
-    const id = this.#nextBufferId++;
-    this.#buffers.set(id, buffer);
-    return id
-  }
-
-  #glBindBuffer(target, bufferId) {
-    const buffer = this.#buffers.get(bufferId);
-    this.#gl.bindBuffer(target, buffer)
-  }
-
-  #glBufferData(target, size, dataPtr, usage) {
-    const data = new Float32Array(
-      this.#wasm.exports.memory.buffer,
-      dataPtr,
-      size / 4
-    );
-    this.#gl.bufferData(target, data, usage)
-  }
-
-  #glGetAttribLocation(programId, namePtr) {
-    const program = this.#programs.get(programId);
-    const name = this.#readCString(namePtr);
-    return this.#gl.getAttribLocation(program, name)
-  }
-
-  #glEnableVertexAttribArray(idx) {
-    this.#gl.enableVertexAttribArray(idx)
-  }
-
-  #glVertexAttribPointer(idx, size, type, normalized, stride, offset) {
-    this.#gl.vertexAttribPointer(idx, size, type, normalized, stride, offset)
-  }
-
-  #glDrawArrays(mode, first, count) {
-    let err = this.#gl.getError();
-    if (err != 0)
-      throw new Error("WebGL error before draw:", err);
-
-    this.#gl.drawArrays(mode, first, count);
-
-    err = this.#gl.getError();
-    if (err != 0)
-      throw new Error("WebGL error after draw:", err);
-  }
-
-  #uniformLocations = new Map();
-  #nextUniformId = 1;
-
-  #glGetUniformLocation(programId, namePtr) {
-    const program = this.#programs.get(programId);
-    const name = this.#readCString(namePtr);
-    const location = this.#gl.getUniformLocation(program, name);
-
-    const id = this.#nextUniformId++;
-    this.#uniformLocations.set(id, location);
-    return id
-    // console.log("glGetUniformLocation name", name);
-    // return this.#gl.getUniformLocation(program, name)
-  }
-
-  #glUniform1i(locationId, value) {
-    console.log("unifLoc", locationId, value);
-    const loc = this.#uniformLocations.get(locationId);
-    this.#gl.uniform1i(loc, value)
-  }
-
-  #glActiveTexture(texture) {
-    this.#gl.activeTexture(texture)
-  }
-
 
   // Game loop
   update() {
--- DEMOS/webgl_demo/webgl.js
+++ DEMOS/webgl_demo/webgl.js
@@ -1,13 +1,206 @@
 class WebGLGame extends Posit92 {
+  /**
+   * @type {WebGLRenderingContext}
+   */
+  #gl;
+
 	async init() {
     await super.init()
+
+    // Important: #ctx initialisation must be turned off
+    this.#gl = this.#canvas.getContext("webgl") ?? this.#canvas.getContext("experimental-webgl");
+
+    if (this.#gl == null)
+      throw new Error("WebGL is not supported!");
   }
+
+  // TODO: Override initWebAssembly
 
   async afterinit() {
     await super.afterinit()
   }
 
   cleanup() { super.cleanup() }
+
+  // Game loop
   update() { super.update() }
   draw() { super.draw() }
+
+  // VGA.PAS
+  /**
+   * Superseded by flushWebGL in Pascal code
+   */
+  flush() { }
+
+  // WEBGL.PAS
+  #readCString(ptr) {
+    this.#assertNumber(ptr);
+
+    const memory = new Uint8Array(this.#wasm.exports.memory.buffer);
+    let end = ptr;
+    while (memory[end] != 0) end++;  // Find null terminator
+
+    const bytes = memory.subarray(ptr, end);
+    return new TextDecoder().decode(bytes)
+  }
+
+  /**
+   * @type {Map<number, WebGLTexture>}
+   */
+  #textures = new Map();
+  #nextTextureId = 1;
+
+  #glCreateTexture() {
+    const texture = this.#gl.createTexture();
+    const id = this.#nextTextureId++;
+    console.log("id", id, this.#nextTextureId);
+    this.#textures.set(id, texture);
+    return 1
+  }
+
+  #glBindTexture(target, textureId) {
+    const texture = this.#textures.get(textureId);
+    this.#gl.bindTexture(target, texture)
+  }
+
+  #glTextParameteri(target, pname, param) {
+    this.#gl.texParameteri(target, pname, param)
+  }
+
+  #glTexImage2D(
+    target, level, internalFormat,
+    width, height, border,
+    format, type, pixelsPtr) {
+
+    const pixels = new Uint8Array(
+      this.#wasm.exports.memory.buffer,
+      pixelsPtr,
+      width * height * 4);
+
+    this.#gl.texImage2D(
+      target, level, internalFormat,
+      width, height, border,
+      format, type, pixels)
+  }
+
+  #shaders = new Map();
+  #nextShaderId = 1;
+  #programs = new Map();
+  #nextProgramId = 1;
+
+  #glCreateShader(type) {
+    const shader = this.#gl.createShader(type);
+    const id = this.#nextShaderId++;
+    this.#shaders.set(id, shader);
+    return id
+  }
+
+  #glShaderSource(shaderId, sourcePtr) {
+    const shader = this.#shaders.get(shaderId);
+    const source = this.#readCString(sourcePtr);
+    this.#gl.shaderSource(shader, source)
+  }
+
+  #glCompileShader(shaderId) {
+    const shader = this.#shaders.get(shaderId);
+    this.#gl.compileShader(shader)
+  }
+
+  #glCreateProgram() {
+    const program = this.#gl.createProgram();
+    const id = this.#nextProgramId++;
+    this.#programs.set(id, program);
+    return id
+  }
+
+  #glAttachShader(programId, shaderId) {
+    const program = this.#programs.get(programId);
+    const shader = this.#shaders.get(shaderId);
+    this.#gl.attachShader(program, shader)
+  }
+
+  #glLinkProgram(programId) {
+    const program = this.#programs.get(programId);
+    this.#gl.linkProgram(program)
+  }
+
+  #glUseProgram(programId) {
+    const program = this.#programs.get(programId);
+    this.#gl.useProgram(program)
+  }
+
+  #buffers = new Map();
+  #nextBufferId = 1;
+
+  #glCreateBuffer() {
+    const buffer = this.#gl.createBuffer();
+    const id = this.#nextBufferId++;
+    this.#buffers.set(id, buffer);
+    return id
+  }
+
+  #glBindBuffer(target, bufferId) {
+    const buffer = this.#buffers.get(bufferId);
+    this.#gl.bindBuffer(target, buffer)
+  }
+
+  #glBufferData(target, size, dataPtr, usage) {
+    const data = new Float32Array(
+      this.#wasm.exports.memory.buffer,
+      dataPtr,
+      size / 4
+    );
+    this.#gl.bufferData(target, data, usage)
+  }
+
+  #glGetAttribLocation(programId, namePtr) {
+    const program = this.#programs.get(programId);
+    const name = this.#readCString(namePtr);
+    return this.#gl.getAttribLocation(program, name)
+  }
+
+  #glEnableVertexAttribArray(idx) {
+    this.#gl.enableVertexAttribArray(idx)
+  }
+
+  #glVertexAttribPointer(idx, size, type, normalized, stride, offset) {
+    this.#gl.vertexAttribPointer(idx, size, type, normalized, stride, offset)
+  }
+
+  #glDrawArrays(mode, first, count) {
+    let err = this.#gl.getError();
+    if (err != 0)
+      throw new Error("WebGL error before draw:", err);
+
+    this.#gl.drawArrays(mode, first, count);
+
+    err = this.#gl.getError();
+    if (err != 0)
+      throw new Error("WebGL error after draw:", err);
+  }
+
+  #uniformLocations = new Map();
+  #nextUniformId = 1;
+
+  #glGetUniformLocation(programId, namePtr) {
+    const program = this.#programs.get(programId);
+    const name = this.#readCString(namePtr);
+    const location = this.#gl.getUniformLocation(program, name);
+
+    const id = this.#nextUniformId++;
+    this.#uniformLocations.set(id, location);
+    return id
+    // console.log("glGetUniformLocation name", name);
+    // return this.#gl.getUniformLocation(program, name)
+  }
+
+  #glUniform1i(locationId, value) {
+    console.log("unifLoc", locationId, value);
+    const loc = this.#uniformLocations.get(locationId);
+    this.#gl.uniform1i(loc, value)
+  }
+
+  #glActiveTexture(texture) {
+    this.#gl.activeTexture(texture)
+  }
 }
