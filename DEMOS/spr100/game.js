bcc1adfe9847d12cc55c90d0c711061933a11466
Hevanafa <dhevansafa4@gmail.com>
2025-12-04 04:39:47 +0700

    added spr100 demo prototype

---
+++ DEMOS/spr100/game.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Asset boilerplate
+class Game extends Posit92 {
+  async loadAssets() {
+    let handle = 0;
+
+    handle = await this.loadImage("assets/images/cursor.png");
+    this.wasmInstance.exports.setImgCursor(handle);
+
+    await this.loadBMFont(
+      "assets/fonts/nokia_cellphone_fc_8.txt",
+      this.wasmInstance.exports.defaultFontPtr(),
+      this.wasmInstance.exports.defaultFontGlyphsPtr());
+
+    handle = await this.loadImage("assets/images/dosu_1.png");
+    this.wasmInstance.exports.setImgDosuEXE(handle, 0);
+    handle = await this.loadImage("assets/images/dosu_2.png");
+    this.wasmInstance.exports.setImgDosuEXE(handle, 1);
+
+    // Add more assets as necessary
+  }
+}
+
+const TargetFPS = 60;
+const FrameTime = 1000 / 60.0;
+/**
+ * in milliseconds
+ */
+let lastFrameTime = 0.0;
+
+var done = false;
+
+async function main() {
+  const game = new Game("game");
+  await game.init();
+  game.afterInit();
+
+  function loop(currentTime) {
+    if (done) {
+      game.cleanup();
+      return;
+    }
+
+    const elapsed = currentTime - lastFrameTime;
+
+    if (elapsed >= FrameTime) {
+      lastFrameTime = currentTime - (elapsed % FrameTime);  // Carry over extra time
+      game.update();
+      game.draw();
+    }
+
+    requestAnimationFrame(loop)
+  }
+
+  requestAnimationFrame(loop)
+}
+
+function play() {
+  const overlay = document.getElementById("play-overlay");
+  overlay.parentNode.removeChild(overlay)
+
+  main()
+}
