4ec643d5cc30261793cbe6c48d9bdd479555f381
Hevanafa <dhevansafa4@gmail.com>
2025-12-03 10:36:14 +0700

    added the complete example section

--- snippets/lerp_timer.pas
+++ snippets/lerp_timer.pas
@@ -5,6 +5,7 @@
   This snippet demonstrates how to use a basic TLerpTimer pattern
 }
 
+
 { Step 1: Create a pair of start & end values to interpolate }
 var
   startX, endX: integer;
@@ -31,5 +32,31 @@ end;
 { in init }
 initLerp(XLerpTimer, gameTime, 2.0);
 
+{ in render logic (draw) }
 perc := getLerpPerc(XLerpTimer, gameTime);
 x := lerpEaseOutQuad(startX, endX, perc);
+
+
+{ === COMPLETE EXAMPLE === }
+
+uses Lerp;
+
+var
+  startX, endX: integer;
+  XLerpTimer: TLerpTimer;
+
+procedure init;
+begin
+  startX := 50;
+  endX := 270;
+  initLerp(XLerpTimer, getTimer, 2.0);
+end;
+
+procedure draw;
+var
+  perc, x: double;
+begin
+  perc := getLerpPerc(XLerpTimer, getTimer);
+  x := lerpEaseOutQuad(startX, endX, perc);
+  spr(imgYourSprite, trunc(x), 100)
+end;
