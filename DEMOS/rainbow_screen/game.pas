69e3bf7b2d3778ea300b4f38a9c788eba086eb61
Hevanafa <dhevansafa4@gmail.com>
2025-12-05 01:39:18 +0700

    added rainbow render prototype

--- DEMOS/rainbow_screen/game.pas
+++ DEMOS/rainbow_screen/game.pas
@@ -1,9 +1,9 @@
 library Game;
 
-{$Mode ObjFPC}
+{$Mode TP}
 
 uses
-  Keyboard, Mouse,
+  Keyboard, Mouse, Maths,
   ImgRef, ImgRefFast,
   Timing, VGA,
   Assets;
@@ -11,6 +11,7 @@ uses
 const
   SC_ESC = $01;
   SC_SPACE = $39;
+  White = $FFFFFFFF;
 
 var
   lastEsc: boolean;
@@ -24,6 +25,48 @@ procedure signalDone; external 'env' name 'signalDone';
 procedure drawMouse;
 begin
   spr(imgCursor, mouseX, mouseY)
+end;
+
+{ h, s, v: [0.0, 1.0] }
+function HSVtoRGB(h, s, v: double): longword;
+var
+  r, g, b: byte;
+  i: integer;
+  f, p, q, t: double;
+begin
+  h := clamp(h, 0.0, 1.0);
+  s := clamp(s, 0.0, 1.0);
+  v := clamp(v, 0.0, 1.0);
+
+  { Greyscale }
+  if s = 0.0 then begin
+    r := trunc(v * 255);
+    g := r;
+    b := r;
+    HSVtoRGB := $FF000000 or (r shl 16) or (g shl 8) or b;
+    exit
+  end;
+
+  { Convert hue to [0.0, 6.0] }
+  h := h * 6.0;
+  i := trunc(h);
+  f := h - i;
+
+  p := v * (1.0 - s);
+  q := v * (1.0 - s * f);
+  t := v * (1.0 - s * (1.0 - f));
+
+  { Determine RGB }
+  case i mod 6 of
+    0: begin r := trunc(v * 255); g := trunc(t * 255); b := trunc(p * 255); end;
+    1: begin r := trunc(q * 255); g := trunc(v * 255); b := trunc(p * 255); end;
+    2: begin r := trunc(p * 255); g := trunc(v * 255); b := trunc(t * 255); end;
+    3: begin r := trunc(p * 255); g := trunc(q * 255); b := trunc(v * 255); end;
+    4: begin r := trunc(t * 255); g := trunc(p * 255); b := trunc(v * 255); end;
+    5: begin r := trunc(v * 255); g := trunc(p * 255); b := trunc(q * 255); end;
+  end;
+
+  HSVtoRGB := $FF000000 or (r shl 16) or (g shl 8) or b
 end;
 
 
@@ -58,8 +101,16 @@ procedure draw;
 var
   w: integer;
   s: string;
+  a, b: word;
+  colour: longword;
 begin
-  cls($FF6495ED);
+  { cls($FF6495ED); }
+
+  for b:=0 to vgaHeight - 1 do
+  for a:=0 to vgaWidth - 1 do begin
+    colour := HSVtoRGB(a / (vgaWidth), 1.0, 1.0 - b / (vgaHeight));
+    unsafePset(a, b, colour)
+  end;
 
   if (trunc(gameTime * 4) and 1) > 0 then
     spr(imgDosuEXE[1], 148, 88)
