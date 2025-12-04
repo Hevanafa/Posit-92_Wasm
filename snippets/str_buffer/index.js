6fd26ffec705f287adcd60891e35696c68702ba4
Hevanafa <dhevansafa4@gmail.com>
2025-12-05 02:07:50 +0700

    added documentWrite prototype

--- snippets/str_buffer/index.js
+++ snippets/str_buffer/index.js
@@ -1,17 +1,45 @@
+/**
+ * @type {WebAssembly.Instance}
+ */
+let wasm;
+
+/**
+ * Pass a JS string to Pascal
+ */
+function loadStringBuffer(text) {
+  const encoder = new TextEncoder();
+  const bytes = encoder.encode(text);
+
+  const bufferPtr = wasm.exports.getStringBuffer();
+  const buffer = new Uint8Array(wasm.exports.memory.buffer, bufferPtr, bytes.length);
+  buffer.set(bytes);
+
+  return bytes.length
+}
+
+function documentWrite() {
+  // Read from string buffer, then output as HTML
+  const bufferPtr = wasm.exports.getStringBuffer();
+  const buffer = new Uint8Array(wasm.exports.memory.buffer, bufferPtr, 256);
+
+  // Pascal string starts from index 1
+  const len = buffer[0];
+  const bytes = buffer.slice(1, 1 + len);
+  const msg = new TextDecoder().decode(bytes);
+
+  document.write(msg)
+}
+
 // Add your Pascal external procedures & functions here:
 const importObject = Object.freeze({
   env: {
     _haltproc: exitcode => console.log("Programme halted with code:", exitcode),
 
-    helloWorld: () => console.log("Hello from snippets!")
+    hello: () => console.log("Hello from Wasm!"),
+    documentWrite: () => documentWrite()
     // Add more externals below
   }
 });
-
-/**
- * @type {WebAssembly.Instance}
- */
-let wasm;
 
 async function initWebAssembly() {
   const response = await fetch("main.wasm");
@@ -23,6 +51,7 @@ async function initWebAssembly() {
 async function main() {
   await initWebAssembly();
   wasm.exports.init();
+  // loadStringBuffer("Hello from JS!");
 }
 
 main()
--- snippets/str_buffer/main.pas
+++ snippets/str_buffer/main.pas
@@ -1,8 +1,21 @@
 library StrBuffer;
 
+var
+  stringBuffer: array[0..255] of byte;
+
+procedure hello; external 'env' name 'hello';
+procedure documentWrite; external 'env' name 'documentWrite';
+
+function getStringBuffer: pointer; public name 'getStringBuffer';
+begin
+  getStringBuffer := @stringBuffer
+end;
+
 procedure init;
 begin
-
+  fillchar(stringBuffer, 255, 0);
+  { hello }
+  documentWrite('Hello from Pascal!')
 end;
 
 exports
