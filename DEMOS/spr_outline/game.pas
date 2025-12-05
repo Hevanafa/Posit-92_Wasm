374bfb67694e277bf9a2900be6000e93f5f0aa8f
Hevanafa <dhevansafa4@gmail.com>
2025-12-05 16:45:51 +0700

    added RGB outline

--- DEMOS/spr_outline/game.pas
+++ DEMOS/spr_outline/game.pas
@@ -5,7 +5,7 @@ library Game;
 
 uses
   Keyboard, Mouse,
-  ImgRef, ImgRefFast,
+  ImgRef, ImgRefFast, Maths,
   Timing, VGA,
   Assets;
 
@@ -85,6 +85,48 @@ begin
   spr(imgHandle, x, y)
 end;
 
+{ h, s, v: [0.0, 1.0] }
+function HSVtoRGB(h, s, v: double): longword;
+var
+  r, g, b: byte;
+  i: integer;
+  f, p, q, t: double;
+begin
+  h := clamp(h, 0.0, 1.0);
+  s := clamp(s, 0.0, 1.0);
+  v := clamp(v, 0.0, 1.0);
+
+  { Greyscale }
+  if s = 0.0 then begin
+    r := trunc(v * 255);
+    g := r;
+    b := r;
+    HSVtoRGB := $FF000000 or (r shl 16) or (g shl 8) or b;
+    exit
+  end;
+
+  { Convert hue to [0.0, 6.0] }
+  h := h * 6.0;
+  i := trunc(h);
+  f := h - i;
+
+  p := v * (1.0 - s);
+  q := v * (1.0 - s * f);
+  t := v * (1.0 - s * (1.0 - f));
+
+  { Determine RGB }
+  case i mod 6 of
+    0: begin r := trunc(v * 255); g := trunc(t * 255); b := trunc(p * 255); end;
+    1: begin r := trunc(q * 255); g := trunc(v * 255); b := trunc(p * 255); end;
+    2: begin r := trunc(p * 255); g := trunc(v * 255); b := trunc(t * 255); end;
+    3: begin r := trunc(p * 255); g := trunc(q * 255); b := trunc(v * 255); end;
+    4: begin r := trunc(t * 255); g := trunc(p * 255); b := trunc(v * 255); end;
+    5: begin r := trunc(v * 255); g := trunc(p * 255); b := trunc(q * 255); end;
+  end;
+
+  HSVtoRGB := $FF000000 or (r shl 16) or (g shl 8) or b
+end;
+
 
 procedure init;
 begin
@@ -115,15 +157,24 @@ end;
 
 procedure draw;
 var
-  w: integer;
+  a: integer;
+  hue: double;
+  outline: longword;
+
   s: string;
+  w: integer;
 begin
   cls($FF6495ED);
 
-  if (trunc(gameTime * 4) and 1) > 0 then
-    sprOutline(imgDosuEXE[1], 148, 88, green)
-  else
-    sprOutline(imgDosuEXE[0], 148, 88, green);
+  hue := getTimer - trunc(getTimer);
+  outline := HSVtoRGB(hue, 1.0, 1.0);
+
+  for a:=-1 to 1 do begin
+    if (trunc(gameTime * 4) and 1) > 0 then
+      sprOutline(imgDosuEXE[1], 148 + a * 40, 88, outline)
+    else
+      sprOutline(imgDosuEXE[0], 148 + a * 40, 88, outline);
+  end;
 
   s := 'Hello world!';
   w := measureDefault(s);
