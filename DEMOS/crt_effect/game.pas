9fbbcc09a67f27d011505f2b332acab6fd63dfc8
Hevanafa <dhevansafa4@gmail.com>
2025-12-06 12:41:57 +0700

    added flickering background

--- DEMOS/crt_effect/game.pas
+++ DEMOS/crt_effect/game.pas
@@ -3,7 +3,8 @@ library Game;
 {$Mode TP}
 
 uses
-  Conv, FPS, Keyboard, Mouse,
+  Conv, FPS, Maths,
+  Keyboard, Mouse,
   ImgRef, ImgRefFast,
   PostProc, Timing, VGA,
   Assets;
@@ -33,6 +34,48 @@ end;
 procedure drawFPS;
 begin
   printDefault('FPS:' + i32str(getLastFPS), 240, 0);
+end;
+
+{ h, s, v: [0.0, 1.0] }
+function HSVtoRGB(h, s, v: double): longword;
+var
+  r, g, b: byte;
+  i: integer;
+  f, p, q, t: double;
+begin
+  h := clamp(h, 0.0, 1.0);
+  s := clamp(s, 0.0, 1.0);
+  v := clamp(v, 0.0, 1.0);
+
+  { Greyscale }
+  if s = 0.0 then begin
+    r := trunc(v * 255);
+    g := r;
+    b := r;
+    HSVtoRGB := $FF000000 or (r shl 16) or (g shl 8) or b;
+    exit
+  end;
+
+  { Convert hue to [0.0, 6.0] }
+  h := h * 6.0;
+  i := trunc(h);
+  f := h - i;
+
+  p := v * (1.0 - s);
+  q := v * (1.0 - s * f);
+  t := v * (1.0 - s * (1.0 - f));
+
+  { Determine RGB }
+  case i mod 6 of
+    0: begin r := trunc(v * 255); g := trunc(t * 255); b := trunc(p * 255); end;
+    1: begin r := trunc(q * 255); g := trunc(v * 255); b := trunc(p * 255); end;
+    2: begin r := trunc(p * 255); g := trunc(v * 255); b := trunc(t * 255); end;
+    3: begin r := trunc(p * 255); g := trunc(q * 255); b := trunc(v * 255); end;
+    4: begin r := trunc(t * 255); g := trunc(p * 255); b := trunc(v * 255); end;
+    5: begin r := trunc(v * 255); g := trunc(p * 255); b := trunc(q * 255); end;
+  end;
+
+  HSVtoRGB := $FF000000 or (r shl 16) or (g shl 8) or b
 end;
 
 
@@ -67,10 +110,18 @@ end;
 
 procedure draw;
 var
-  w: integer;
-  s: string;
+  bg: longword;
+  randomCoeff: double;
+  brightness, strength: double;
 begin
-  cls(DarkGreen);
+  { Assign a randomised coefficient }
+  { brightness := 1.0 + sin(getTimer * 5.0) * 0.2; }
+  randomCoeff := random - 0.5;
+  brightness := 1.0 + randomCoeff * 0.3;
+
+  bg := HSVtoRGB(1 / 3.0, 1.0, 0.4 + 0.27 * (0.5 + randomCoeff));
+  cls(bg);
+  { cls(DarkGreen); }
 
   spr(imgPipBoy,
     (vgaWidth - getImageWidth(imgPipBoy)) div 2,
@@ -83,9 +134,9 @@ begin
   applyFullPhosphor(1);
   applyFullChromabe;
   applyFullSubtleScanlines;
-  
-  { This one's bugged }
-  applyFullVignette(FalloffTypeEaseOutQuad, 0.4);
+
+  strength := 0.4 * brightness;
+  applyFullVignette(FalloffTypeEaseOutQuad, strength);
 
   flush
 end;
