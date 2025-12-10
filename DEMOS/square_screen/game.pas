a65b7e8e95306bb960cc09bed4fad65bc373155b
Hevanafa <dhevansafa4@gmail.com>
2025-12-10 21:21:40 +0700

    added square screen demo prototype

---
+++ DEMOS/square_screen/game.pas
@@ -0,0 +1,87 @@
+library Game;
+
+{$Mode ObjFPC}
+
+uses
+  Keyboard, Mouse,
+  ImgRef, ImgRefFast,
+  Timing, VGA,
+  Assets;
+
+const
+  SC_ESC = $01;
+  SC_SPACE = $39;
+
+var
+  lastEsc: boolean;
+
+  { Init your game state here }
+  gameTime: double;
+
+{ Use this to set `done` to true }
+procedure signalDone; external 'env' name 'signalDone';
+
+procedure drawMouse;
+begin
+  spr(imgCursor, mouseX, mouseY)
+end;
+
+
+procedure init;
+begin
+  initBuffer;
+  initDeltaTime;
+end;
+
+procedure afterInit;
+begin
+  { Initialise game state here }
+  hideCursor;
+end;
+
+procedure update;
+begin
+  updateDeltaTime;
+
+  updateMouse;
+
+  { Your update logic here }
+  if lastEsc <> isKeyDown(SC_ESC) then begin
+    lastEsc := isKeyDown(SC_ESC);
+    if lastEsc then signalDone;
+  end;
+
+  gameTime := gameTime + dt
+end;
+
+procedure draw;
+var
+  w: integer;
+  s: string;
+begin
+  cls($FF6495ED);
+
+  if (trunc(gameTime * 4) and 1) > 0 then
+    spr(imgDosuEXE[1], 148, 88)
+  else
+    spr(imgDosuEXE[0], 148, 88);
+
+  s := 'Hello world!';
+  w := measureDefault(s);
+  printDefault(s, (vgaWidth - w) div 2, 120);
+
+  drawMouse;
+  vgaFlush
+end;
+
+exports
+  { Main game procedures }
+  init,
+  afterInit,
+  update,
+  draw;
+
+begin
+{ Starting point is intentionally left empty }
+end.
+
