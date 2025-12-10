574f2cb1b9aeb865a2a2232488132ba1360b087d
Hevanafa <dhevansafa4@gmail.com>
2025-12-10 12:36:14 +0700

    fixed lineThick render logic

--- DEMOS/graphics_ex/game.pas
+++ DEMOS/graphics_ex/game.pas
@@ -99,7 +99,7 @@ begin
   arc(160, 100, 50, 0, pi, $FFFF5555);
   pie(160, 100, 50, pi / 4, 3 * pi / 4, $FF55FF55);
 
-  lineThick(50, 50, 200, 150, 5, $FFFF5555);
+  lineThick(50, 50, 200, 150, 15, $FFFF5555);
 
   drawMouse;
   vgaFlush
