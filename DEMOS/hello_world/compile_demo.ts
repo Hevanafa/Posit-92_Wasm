cb5d463f01e21b6cc0c320f0d005bc18c1a42982
Hevanafa <dhevansafa4@gmail.com>
2025-12-12 05:57:14 +0700

    added compile_demo script prototype

---
+++ DEMOS/hello_world/compile_demo.ts
@@ -0,0 +1,29 @@
+import { styleText } from "node:util";
+
+const compilerPath = "E:\\fpc-wasm\\fpc\\bin\\x86_64-win64\\fpc.exe";
+const primaryUnit = ".\\game.pas";
+const outputFile = "game.wasm";
+
+const proc = Bun.spawn([
+  compilerPath,
+  "-Pwasm32",
+  "-Tembedded",
+  "-Fu..\\..\\experimental\\units",
+  "-dWASM",
+  "-o$outputFile",
+  primaryUnit
+], {
+  stdout: "pipe",
+  stderr: "pipe"
+});
+
+const stdout = await new Response(proc.stdout).text();
+const stderr = await new Response(proc.stderr).text();
+const exitCode = await proc.exited;
+
+console.log(styleText("cyan", "(STDOUT)"));
+console.log(stdout.trim() || styleText("gray", "(No data)"));
+
+
+
+export {}
