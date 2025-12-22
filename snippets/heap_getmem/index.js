be3f62dee892f6ba2e24bab72885c78d01116732
Hevanafa <dhevansafa4@gmail.com>
2025-12-22 16:14:38 +0700

    added a proper wasm memory grow logic

--- snippets/heap_getmem/index.js
+++ snippets/heap_getmem/index.js
@@ -18,6 +18,25 @@ async function initWebAssembly() {
   const bytes = await response.arrayBuffer();
   const result = await WebAssembly.instantiate(bytes, importObject);
   wasm = result.instance;
+  
+  /**
+   * Grow Wasm memory size (DOS-style: fixed allocation)
+   * Layout:
+   * * 0-1 MB: stack / globals
+   * * 1MB-2MB: heap
+   */
+
+  const heapStart = 1048576;  // 1 MB = 1024 * 1024 B
+  const heapSize = 1 * 1048576;
+
+  // Wasm memory is in 64KB pages
+  const pages = wasm.exports.memory.buffer.byteLength / 65536;
+  const requiredPages = Math.ceil((heapStart + heapSize) / 65536);
+
+  if (pages < requiredPages)
+    wasm.exports.memory.grow(requiredPages - pages);
+
+  wasm.exports.initHeap(heapStart, heapSize);
 }
 
 async function main() {
