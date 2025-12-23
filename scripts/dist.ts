0d8a3c95da70f57ae858abefdb81f6c775a8e036
Hevanafa <dhevansafa4@gmail.com>
2025-12-23 12:03:34 +0700

    finalised dist script

--- scripts/dist.ts
+++ scripts/dist.ts
@@ -1,17 +1,29 @@
-import { exists, mkdir, rm } from "node:fs/promises";
-import { copyFile, cp } from "node:fs/promises";
+import { mkdir, rm, copyFile, cp } from "node:fs/promises";
+import { existsSync } from "node:fs";
+import { join } from "node:path";
 import { styleText } from "node:util";
 
 const wasmFile = "game.wasm";
 const distDir = "dist";
 
-const filesToCopy = ["game.js", wasmFile, "posit-92.js", "index.html"];
-
-if (!await exists(wasmFile)) {
+if (!existsSync(wasmFile)) {
   console.log(styleText("red", `Missing ${wasmFile}!`));
   process.exit(1)
 }
 
-// TODO: Handle dist dir
-// TODO: Copy files
-// TODO: Copy assets folder
+if (existsSync(distDir))
+  await rm(distDir, { recursive: true });
+
+await mkdir(distDir);
+
+// Copy base files
+const filesToCopy = ["game.js", wasmFile, "posit-92.js", "index.html"];
+
+for (const file of filesToCopy)
+  await copyFile(file, join(distDir, file));
+
+// Assets folder
+if (existsSync("assets"))
+  await cp("assets", join(distDir, "assets"), { recursive: true });
+
+console.log(styleText("green", `Files copied to ${distDir} successfully!`))
