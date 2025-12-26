fb3b1dcfca80ede3d735824785cbe4e8f0795df4
Hevanafa <dhevansafa4@gmail.com>
2025-12-26 17:54:54 +0700

    added manifest reader prototype

--- snippets/gen_assets/generate_assets.ts
+++ snippets/gen_assets/generate_assets.ts
@@ -1,5 +1,20 @@
+import { existsSync } from "node:fs";
+import { styleText } from "node:util";
 
+const manifestFile = "assets.json";
 
-await Bun.write("AddAssets.pas", "Hello");
+if (!existsSync(manifestFile)) {
+  console.log(styleText("red", "Missing " + manifestFile + "!"));
+  process.exit(1)
+}
+
+const manifest = await Bun.file(manifestFile).json();
+
+for (const key in manifest.images) {
+  const path = manifest.images[key];
+  console.log(key, path);
+}
+
+// await Bun.write("AddAssets.pas", "Hello");
 
 export {}
