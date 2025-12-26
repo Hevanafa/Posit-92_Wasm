41d9d25faa4c6926211879a556eace666432087e
Hevanafa <dhevansafa4@gmail.com>
2025-12-26 18:00:41 +0700

    added variable generator

--- snippets/gen_assets/generate_assets.ts
+++ snippets/gen_assets/generate_assets.ts
@@ -8,13 +8,33 @@ if (!existsSync(manifestFile)) {
   process.exit(1)
 }
 
+function capitalise(text) { return text.replace(/^(.)/, (_, g1) => g1.toUpperCase()) }
+
 const manifest = await Bun.file(manifestFile).json();
+
+const
+  pascalVariables = [],
+  pascalInterface = [],
+  pascalImplementation = [];
 
 for (const key in manifest.images) {
   const path = manifest.images[key];
-  console.log(key, path);
+
+  const varname = "img" + capitalise(key)
+    .replace(/_(.)/g, (_, g1) => g1.toUpperCase());
+
+  pascalVariables.push(varname)
+  // console.log(key, path);
 }
 
-// await Bun.write("AddAssets.pas", "Hello");
+// TODO: Write everything in one go
+await Bun.write(
+  "AddAssets.pas",
+`
+interface
+
+var
+${pascalVariables.map(item => "  " + item).join(",\r\n")}: longint;
+`);
 
 export {}
