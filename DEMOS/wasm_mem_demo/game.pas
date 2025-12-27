66c85dffa1b236598fe98322991effdbf006cf9d
Hevanafa <dhevansafa4@gmail.com>
2025-12-27 18:20:03 +0700

    added wasm_mem_demo prototype

---
+++ DEMOS/wasm_mem_demo/game.pas
@@ -0,0 +1,104 @@
+library Game;
+
+{$Mode ObjFPC}
+
+uses
+  Conv, FPS, Loading, Logger,
+  Keyboard, Mouse,
+  ImgRef, ImgRefFast,
+  PostProc, Timing, WasmMemMgr, VGA,
+  Assets;
+
+const
+  SC_ESC = $01;
+  SC_SPACE = $39;
+
+var
+  lastEsc: boolean;
+
+  { Init your game state here }
+  gameTime: double;
+
+{ Use this to set `done` to true }
+procedure signalDone; external 'env' name 'signalDone';
+
+procedure drawFPS;
+begin
+  printDefault('FPS:' + i32str(getLastFPS), 240, 0);
+end;
+
+procedure drawMouse;
+begin
+  spr(imgCursor, mouseX, mouseY)
+end;
+
+
+procedure init;
+begin
+  initMemMgr;
+  initBuffer;
+  initDeltaTime;
+  initFPSCounter;
+end;
+
+procedure afterInit;
+begin
+  { Initialise game state here }
+  hideCursor;
+
+  replaceColours(defaultFont.imgHandle, $FFFFFFFF, $FF000000);
+end;
+
+procedure update;
+begin
+  updateDeltaTime;
+  incrementFPS;
+
+  updateMouse;
+
+  { Your update logic here }
+  if lastEsc <> isKeyDown(SC_ESC) then begin
+    lastEsc := isKeyDown(SC_ESC);
+
+    if lastEsc then begin
+      writeLog('ESC is pressed!');
+      signalDone
+    end;
+  end;
+
+  gameTime := gameTime + dt
+end;
+
+procedure draw;
+var
+  w: integer;
+  s: string;
+begin
+  cls($FF6495ED);
+
+  if (trunc(gameTime * 4) and 1) > 0 then
+    spr(imgDosuEXE[1], 148, 88)
+  else
+    spr(imgDosuEXE[0], 148, 88);
+
+  s := 'Hello world!';
+  w := measureDefault(s);
+  printDefault(s, (vgaWidth - w) div 2, 120);
+
+  drawMouse;
+  drawFPS;
+
+  vgaFlush
+end;
+
+exports
+  { Main game procedures }
+  init,
+  afterInit,
+  update,
+  draw;
+
+begin
+{ Starting point is intentionally left empty }
+end.
+
