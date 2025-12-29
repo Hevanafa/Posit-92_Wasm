cc682f386140b0040d533571d84567025548b82d
Hevanafa <dhevansafa4@gmail.com>
2025-12-29 14:48:43 +0700

    increased text label wrap capacity per line

--- experimental/units/PanicDisplay.pas
+++ experimental/units/PanicDisplay.pas
@@ -9,14 +9,22 @@ procedure panicHaltWithDisplay(const msg: string);
 
 implementation
 
-uses Panic;
+uses ImmedGUI, Panic, VGA;
 
 procedure panicHaltWithDisplay(const msg: string);
 var
   a: word;
   msgBuffer: array[0..255] of byte;
 begin
+  cls($FF550000);
   { TODO: Handle display }
+
+  TextLabel('PANIC', 10, 10);
+
+  TextLabelWrap(msg, 10, 30, vgaWidth - 20);
+
+
+  vgaFlush;
 
   { Prepare string buffer }
   for a:=1 to length(msg) do
