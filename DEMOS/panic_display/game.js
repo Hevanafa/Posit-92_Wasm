353f01a641a9eede89c04f268b280006b62808cb
Hevanafa <dhevansafa4@gmail.com>
2025-12-29 13:11:53 +0700

    added panic display demo prototype

---
+++ DEMOS/panic_display/game.js
@@ -0,0 +1,100 @@
+"use strict";
+
+class Game extends Posit92 {
+  /**
+   * KeyboardEvent.code to DOS scancode
+   */
+  ScancodeMap = {
+    "Escape": 0x01,
+    "Space": 0x39,
+    "Enter": 0x1C,
+    // Add more scancodes as necessary
+  };
+
+  AssetManifest = {
+    images: {
+      cursor: "assets/images/cursor.png"
+      // Add more image assets here
+    }
+  }
+
+  async loadDefaultFont() {
+    await this.loadBMFont(
+      "assets/fonts/nokia_cellphone_fc_8.txt",
+      this.wasmInstance.exports.defaultFontPtr(),
+      this.wasmInstance.exports.defaultFontGlyphsPtr());
+  }
+
+  /**
+   * @override
+   */
+  async loadAssets() {
+    let handle = 0;
+
+    this.initLoadingScreen();
+
+    await this.loadImagesFromManifest(this.AssetManifest.images);
+    // Sounds can be loaded later
+
+    handle = await this.loadImage("assets/images/dosu_1.png");
+    this.wasmInstance.exports.setImgDosuEXE(handle, 0);
+    handle = await this.loadImage("assets/images/dosu_2.png");
+    this.wasmInstance.exports.setImgDosuEXE(handle, 1);
+
+    // Add more assets as necessary
+  }
+
+  async loadIntro() {
+    this.wasmInstance.exports.setImgPosit92Logo(
+      await this.loadImage("assets/images/posit-92_32px.png"));
+
+    this.wasmInstance.exports.setImgFPCLogo(
+      await this.loadImage("assets/images/fpc_logo.png"));
+
+    this.wasmInstance.exports.setImgWasmLogo(
+      await this.loadImage("assets/images/wasm_logo.png"));
+  }
+}
+
+const TargetFPS = 60;
+const FrameTime = 1000 / 60.0;
+/**
+ * in milliseconds
+ */
+let lastFrameTime = 0.0;
+
+var done = false;
+
+async function main() {
+  const game = new Game("game");
+  await game.init();
+  await game.loadDefaultFont();
+
+  game.quickStart();
+
+  function loop(currentTime) {
+    if (done) {
+      game.cleanup();
+      return;
+    }
+
+    const elapsed = currentTime - lastFrameTime;
+
+    if (elapsed >= FrameTime) {
+      lastFrameTime = currentTime - (elapsed % FrameTime);  // Carry over extra time
+      game.update();
+      game.draw();
+    }
+
+    requestAnimationFrame(loop)
+  }
+
+  requestAnimationFrame(loop)
+}
+
+function play() {
+  const overlay = document.getElementById("play-overlay");
+  overlay.parentNode.removeChild(overlay);
+
+  main()
+}
---
+++ DEMOS/panic_display/game.pas
@@ -0,0 +1,143 @@
+library Game;
+
+{$Mode ObjFPC}
+{$J-}  { Switch off assignments to typed constants }
+
+uses
+  Loading,
+  Conv, FPS, Logger,
+  Keyboard, Mouse,
+  ImgRef, ImgRefFast,
+  PanicDisplay, Timing, WasmHeap, WasmMemMgr,
+  VGA,
+  Assets;
+
+type
+  TGameStates = (
+    GameStateIntro = 1,
+    GameStateLoading = 2,
+    GameStatePlaying = 3
+  );
+
+const
+  SC_ESC = $01;
+  SC_SPACE = $39;
+  SC_ENTER = $1C;
+
+  CornflowerBlue = $FF6495ED;
+
+var
+  lastEsc: boolean;
+  lastSpacebar, lastEnter: boolean;
+
+  { Init your game state here }
+  actualGameState: TGameStates;
+  gameTime: double;
+
+
+{ Use this to set `done` to true }
+procedure signalDone; external 'env' name 'signalDone';
+procedure hideCursor; external 'env' name 'hideCursor';
+procedure hideLoadingOverlay; external 'env' name 'hideLoadingOverlay';
+procedure loadAssets; external 'env' name 'loadAssets';
+
+procedure drawFPS;
+begin
+  printDefault('FPS:' + i32str(getLastFPS), 240, 0);
+end;
+
+procedure drawMouse;
+begin
+  spr(imgCursor, mouseX, mouseY)
+end;
+
+procedure beginLoadingState;
+begin
+  actualGameState := GameStateLoading;
+  loadAssets
+end;
+
+procedure beginPlayingState;
+begin
+  { Initialise game state here }
+  hideCursor;
+  actualGameState := GameStatePlaying;
+  gameTime := 0.0;
+end;
+
+
+procedure init;
+begin
+  initMemMgr;
+  initBuffer;
+  initDeltaTime;
+  initFPSCounter
+end;
+
+procedure afterInit;
+begin
+  beginPlayingState;
+end;
+
+procedure update;
+var
+  shouldSkip: boolean;
+begin
+  updateDeltaTime;
+  incrementFPS;
+
+  { Handle inputs }
+  updateMouse;
+
+  if lastEsc <> isKeyDown(SC_ESC) then begin
+    lastEsc := isKeyDown(SC_ESC);
+
+    if lastEsc then begin
+      writeLog('ESC is pressed!');
+      signalDone
+    end;
+  end;
+
+  { Handle game state updates }
+  gameTime := gameTime + dt
+end;
+
+procedure draw;
+var
+  w: integer;
+  s: string;
+begin
+  if actualGameState = GameStateLoading then begin
+    renderLoadingScreen;
+    exit
+  end;
+
+  cls(CornflowerBlue);
+
+  if (trunc(gameTime * 4) and 1) > 0 then
+    spr(imgDosuEXE[1], 148, 88)
+  else
+    spr(imgDosuEXE[0], 148, 88);
+
+  s := 'Hello world!';
+  w := measureDefault(s);
+  printDefault(s, (vgaWidth - w) div 2, 120);
+
+  drawMouse;
+  drawFPS;
+
+  vgaFlush
+end;
+
+exports
+  beginIntroState,
+  beginLoadingState,
+  init,
+  afterInit,
+  update,
+  draw;
+
+begin
+{ Starting point is intentionally left empty }
+end.
+
