6d9e49c447e01e988f02e9383e429d98de2fa58f
Hevanafa <dhevansafa4@gmail.com>
2025-12-31 06:25:50 +0700

    added gamepad method prototypes

--- DEMOS/gamepad_demo/gamepad.js
+++ DEMOS/gamepad_demo/gamepad.js
@@ -1,4 +1,24 @@
 class GamepadMixin extends Posit92 {
+  #setupImportObject() {
+    const { env } = super._getWasmImportObject();
+
+    Object.assign(env, {
+      gamepadButton: this.#gamepadButton.bind(this),
+      gamepadAxis: this.#gamepadAxis.bind(this)
+    })
+  }
+
+  #gamepadButton() {}
+  #gamepadAxis() {}
+
+  /**
+   * @override
+   */
+  async init() {
+    this.#setupImportObject();
+    await super.init();
+  }
+
   update() {
     
     super.update()
