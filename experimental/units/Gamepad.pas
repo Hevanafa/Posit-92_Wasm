1a1ea3732dd8a42c7432f2e3963eb46ef5a6f1ee
Hevanafa <dhevansafa4@gmail.com>
2025-12-31 09:46:44 +0700

    added gamepad constants

--- experimental/units/Gamepad.pas
+++ experimental/units/Gamepad.pas
@@ -2,6 +2,26 @@ unit Gamepad;
 
 interface
 
+const
+  BTN_A = 0;
+  BTN_B = 1;
+  BTN_X = 2;
+  BTN_Y = 3;
+
+  BTN_LB = 4;
+  BTN_RB = 4;
+  BTN_LT = 4;
+  BTN_RT = 4;
+
+  BTN_BACK = 8;
+  BTN_START = 9;
+
+  BTN_DPAD_UP = 12;
+  BTN_DPAD_DOWN = 13;
+  BTN_DPAD_LEFT = 14;
+  BTN_DPAD_RIGHT = 15;
+
+
 function gamepadButton(const button: byte): boolean; external 'env' name 'gamepadButton';
 function gamepadAxis(const axis: byte): single; external 'env' name 'gamepadAxis';
 
