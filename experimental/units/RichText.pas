1a39b4168983ca7b7e7cf53b94bff13935363357
Hevanafa <dhevansafa4@gmail.com>
2026-01-04 11:03:38 +0700

    added regular font boilerplate

--- experimental/units/RichText.pas
+++ experimental/units/RichText.pas
@@ -2,6 +2,8 @@ unit RichText;
 
 interface
 
+procedure rtfSetRegularFont(const font: TBMFont; const glyphs: array of TBMFontGlyph);
+
 procedure TestRichTextLabel(
   const text: string;
   const x, y: integer;
@@ -9,6 +11,22 @@ procedure TestRichTextLabel(
 
 
 implementation
+
+uses BMFont;
+
+var
+  isFontSet: boolean;
+  regularFont: TBMFont;
+  regularFontGlyphs: array[32..126] of TBMFontGlyph;
+
+procedure rtfSetRegularFont(const font: TBMFont; const glyphs: array of TBMFontGlyph);
+begin
+  isFontSet := true;
+  activeFont := font;
+
+  for a := 32 to 126 do
+    activeFontGlyphs[a] := glyphs[a - 32];
+end;
 
 procedure TestRichTextLabel(
   const text: string;
@@ -18,7 +36,9 @@ var
   bold, italic: boolean;
   currentColour: cardinal;
 begin
-  
+  if not isFontSet then panicHalt('TestRichTextLabel: font is unset!');
+
+
 end;
 
 end.
