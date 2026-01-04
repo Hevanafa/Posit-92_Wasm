245198e5221cc96f356c37f802f2ca6cd64f3e5a
Hevanafa <dhevansafa4@gmail.com>
2026-01-04 10:57:24 +0700

    added rich text unit prototype

---
+++ experimental/units/RichText.pas
@@ -0,0 +1,24 @@
+unit RichText;
+
+interface
+
+procedure TestRichTextLabel(
+  const text: string;
+  const x, y: integer;
+  const colorTable: array of longword);
+
+
+implementation
+
+procedure TestRichTextLabel(
+  const text: string;
+  const x, y: integer;
+  const colorTable: array of longword);
+var
+  bold, italic: boolean;
+  currentColour: cardinal;
+begin
+  
+end;
+
+end.
