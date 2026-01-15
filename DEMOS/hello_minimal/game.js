b6962ca95e8c755de27a294afe6c41c7df2f4535
Hevanafa <dhevansafa4@gmail.com>
2026-01-15 21:56:38 +0700

    trimmed game.js of hello_minimal

--- DEMOS/hello_minimal/game.js
+++ DEMOS/hello_minimal/game.js
@@ -1,22 +1,10 @@
 "use strict";
 
 /**
- * Simple Boilerplate
+ * Minimal Boilerplate
  */
 class Game extends Posit92 {
-  /**
-   * KeyboardEvent.code to DOS scancode
-   */
-  ScancodeMap = {};
 }
-
-const TargetFPS = 60;
-const FrameTime = 1000 / 60.0;
-
-/**
- * in milliseconds
- */
-let lastFrameTime = 0.0;
 
 /**
  * Entry point
@@ -25,38 +13,10 @@ async function main() {
   const game = new Game("game");
   await game.init();
 
-  const wasm = game.wasmInstance;
-  
-  // Available in assets.pas
-  const {
-    defaultFontPtr, defaultFontGlyphsPtr,
-    setImgCursor, setImgDosuEXE
-  } = wasm.exports;
-
-  // Load default font
-  await game.loadBMFont(
-    "assets/fonts/nokia_cellphone_fc_8.txt",
-    defaultFontPtr(), defaultFontGlyphsPtr());
-
-  // Load assets
-  setImgCursor(await game.loadImage("assets/images/cursor.png"));
-  setImgDosuEXE(await game.loadImage("assets/images/dosu_1.png"), 0);
-  setImgDosuEXE(await game.loadImage("assets/images/dosu_2.png"), 1);
-
   game.hideLoadingOverlay();
-  wasm.exports.afterInit();
+  game.wasmInstance.exports.afterInit();
 
-  function loop(currentTime) {
-    const elapsed = currentTime - lastFrameTime;
-
-    if (elapsed >= FrameTime) {
-      lastFrameTime = currentTime - (elapsed % FrameTime);  // Carry over extra time
-      game.update();
-      game.draw();
-    }
-
-    requestAnimationFrame(loop)
-  }
-
-  requestAnimationFrame(loop)
+  // Your game loop here
+  game.update();
+  game.draw();
 }
