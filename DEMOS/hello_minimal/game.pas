072fc3420f95a30e8d8a15f53ee6d7a54de04c9a
Hevanafa <dhevansafa4@gmail.com>
2026-01-15 22:04:53 +0700

    trimmed down game.pas

--- DEMOS/hello_minimal/game.pas
+++ DEMOS/hello_minimal/game.pas
@@ -4,47 +4,25 @@ library Game;
 {$J-}  { Switch off assignments to typed constants }
 
 uses
-  Fullscreen, ImgRefFast, Timing,
-  WasmMemMgr, VGA,
-  Assets;
-
-var
-  { Game state variables }
-  gameTime: double;
+  WasmMemMgr, VGA;
 
 procedure init;
 begin
   initMemMgr;
   initBuffer;
-  initDeltaTime
 end;
 
 procedure afterInit;
 begin
-  fitCanvas;
-
-  { Initialise game state here }
-  gameTime := 0.0;
 end;
 
 procedure update;
 begin
-  updateDeltaTime;
-
-  { Handle game state updates }
-  gameTime := gameTime + dt
 end;
 
 procedure draw;
 begin
-  cls($FF6495ED);
-
-  if (trunc(gameTime * 4) and 1) > 0 then
-    spr(imgDosuEXE[1], 148, 88)
-  else
-    spr(imgDosuEXE[0], 148, 88);
-
-  printDefaultCentred('Hello world!', vgaWidth div 2, 120);
+  cls($FF101010);
 
   vgaFlush
 end;
