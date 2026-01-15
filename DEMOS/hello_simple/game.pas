f7d0423e79008cc7eab78dc7900777bfc7c961ea
Hevanafa <dhevansafa4@gmail.com>
2026-01-15 21:13:11 +0700

    trimmed boilerplate code

--- DEMOS/hello_simple/game.pas
+++ DEMOS/hello_simple/game.pas
@@ -4,55 +4,26 @@ library Game;
 {$J-}  { Switch off assignments to typed constants }
 
 uses
-  Loading, Fullscreen,
-  Conv, FPS, Logger,
-  Keyboard, Mouse,
+  Fullscreen,
   ImgRefFast,
   Timing, WasmMemMgr,
   VGA,
   Assets;
 
-const
-  SC_ESC = $01;
-
-  CornflowerBlue = $FF6495ED;
-
 var
-  lastEsc: boolean;
-
   { Game state variables }
   gameTime: double;
-
-
-{ Use this to set `done` to true }
-procedure signalDone; external 'env' name 'signalDone';
-procedure hideCursor; external 'env' name 'hideCursor';
-procedure hideLoadingOverlay; external 'env' name 'hideLoadingOverlay';
-procedure loadAssets; external 'env' name 'loadAssets';
-
-procedure drawFPS;
-begin
-  printDefault('FPS:' + i32str(getLastFPS), 240, 0);
-end;
-
-procedure drawMouse;
-begin
-  spr(imgCursor, mouseX, mouseY)
-end;
 
 procedure init;
 begin
   initMemMgr;
   initBuffer;
-  initDeltaTime;
-  initFPSCounter
+  initDeltaTime
 end;
 
 procedure afterInit;
 begin
-  hideCursor;
   fitCanvas;
-
   { Initialise game state here }
   gameTime := 0.0;
 end;
@@ -60,42 +31,21 @@ end;
 procedure update;
 begin
   updateDeltaTime;
-  incrementFPS;
-
-  { Handle inputs }
-  updateMouse;
-
-  if lastEsc <> isKeyDown(SC_ESC) then begin
-    lastEsc := isKeyDown(SC_ESC);
-
-    if lastEsc then begin
-      writeLog('ESC is pressed!');
-      signalDone
-    end;
-  end;
 
   { Handle game state updates }
   gameTime := gameTime + dt
 end;
 
 procedure draw;
-var
-  w: integer;
-  s: string;
 begin
-  cls(CornflowerBlue);
+  cls($FF6495ED);
 
   if (trunc(gameTime * 4) and 1) > 0 then
     spr(imgDosuEXE[1], 148, 88)
   else
     spr(imgDosuEXE[0], 148, 88);
 
-  s := 'Hello world!';
-  w := measureDefault(s);
-  printDefault(s, (vgaWidth - w) div 2, 120);
-
-  drawMouse;
-  drawFPS;
+  printDefaultCentred('Hello world!', vgaWidth div 2, 120);
 
   vgaFlush
 end;
