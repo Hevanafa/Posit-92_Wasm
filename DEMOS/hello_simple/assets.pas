edb2a52c3b64d9097299432e14f21a2075483ca1
Hevanafa <dhevansafa4@gmail.com>
2026-01-15 20:50:06 +0700

    added hello_simple demo prototype

---
+++ DEMOS/hello_simple/assets.pas
@@ -0,0 +1,77 @@
+unit Assets;
+
+{$Mode ObjFPC}
+
+interface
+
+uses BMFont;
+
+var
+  { for use in loadBMFont }
+  defaultFont: TBMFont;
+  defaultFontGlyphs: array[32..126] of TBMFontGlyph;
+
+  imgCursor: longint;
+  imgDosuEXE: array[0..1] of longint;
+
+{ BMFont boilerplate }
+function defaultFontPtr: PBMFont; public name 'defaultFontPtr';
+function defaultFontGlyphsPtr: PBMFontGlyph; public name 'defaultFontGlyphsPtr';
+
+procedure printDefault(const text: string; const x, y: integer);
+procedure printDefaultCentred(const text: string; const cx, y: integer);
+function measureDefault(const text: string): word;
+
+{ Asset boilerplate }
+procedure setImgCursor(const imgHandle: longint); public name 'setImgCursor';
+procedure setImgDosuEXE(const imgHandle: longint; const idx: integer); public name 'setImgDosuEXE';
+
+
+implementation
+
+uses Conv;
+
+{ Begin BMFont boilerplate}
+
+function defaultFontPtr: PBMFont;
+begin
+  defaultFontPtr := @defaultFont
+end;
+
+function defaultFontGlyphsPtr: PBMFontGlyph;
+begin
+  defaultFontGlyphsPtr := @defaultFontGlyphs
+end;
+
+procedure printDefault(const text: string; const x, y: integer);
+begin
+  printBMFont(defaultFont, defaultFontGlyphs, text, x, y)
+end;
+
+procedure printDefaultCentred(const text: string; const cx, y: integer);
+var
+  w: word;
+begin
+  w := measureDefault(text);
+  printDefault(text, cx - w div 2, y)
+end;
+
+function measureDefault(const text: string): word;
+begin
+  measureDefault := measureBMFont(defaultFontGlyphs, text)
+end;
+
+
+{ Begin asset boilerplate }
+
+procedure setImgCursor(const imgHandle: longint);
+begin
+  imgCursor := imgHandle
+end;
+
+procedure setImgDosuEXE(const imgHandle: longint; const idx: integer);
+begin
+  imgDosuEXE[idx] := imgHandle
+end;
+
+end.
