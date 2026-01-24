f0bc131c7552f033f3b2de3bcff8b2b7f009fb53
Hevanafa <dhevansafa4@gmail.com>
2026-01-24 10:47:50 +0700

    added movement blueprint

--- experimental/blueprints/movement/game.pas
+++ experimental/blueprints/movement/game.pas
@@ -17,13 +17,29 @@ var
   playerZone: TZone;
 
 
+procedure beginPlayingState;
+begin
+  playerZone.x := 100;
+  playerZone.y := 100;
+  playerZone.width := 12;
+  playerZone.height := 8;
+end;
+
 
 procedure update;
 begin
+  updateDeltaTime;
+
   if isKeyDown(SC_W) then playerZone.y := playerZone.y - Velocity * dt;
   if isKeyDown(SC_S) then playerZone.y := playerZone.y + Velocity * dt;
   if isKeyDown(SC_A) then playerZone.x := playerZone.x - Velocity * dt;
   if isKeyDown(SC_D) then playerZone.x := playerZone.x + Velocity * dt;
+end;
+
+
+procedure draw;
+begin
+  drawZone(playerZone, white);
 end;
 
 
