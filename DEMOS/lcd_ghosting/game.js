2a05fb6409ae192eca122b4fe14606fd7590ac91
Hevanafa <dhevansafa4@gmail.com>
2026-03-16 13:05:29 +0700

    fourth attempt to do LCD ghosting

--- DEMOS/lcd_ghosting/game.js
+++ DEMOS/lcd_ghosting/game.js
@@ -79,15 +79,18 @@ class Game extends Posit92 {
      */
     let accumulatorCtx;
 
-    // Do the surface copy (snapshot)
+    // Surface copy (snapshot)
     accumulatorCtx = this.accumulatorSurfaceCopy.getContext("2d");
+    // accumulatorCtx.clearRect(0, 0, this.vgaWidth, this.vgaHeight);
     accumulatorCtx.drawImage(this.accumulatorSurface, 0, 0);
 
     // Draw the copy back with the decay alpha
     accumulatorCtx = this.accumulatorSurface.getContext("2d");
-    accumulatorCtx.clearRect(0, 0, this.vgaWidth, this.vgaHeight);
-    accumulatorCtx.globalAlpha = 1 / 30.0;
+    // accumulatorCtx.clearRect(0, 0, this.vgaWidth, this.vgaHeight);
     accumulatorCtx.drawImage(this.accumulatorSurfaceCopy, 0, 0);
+    // accumulatorCtx.globalAlpha = 0.9;
+    accumulatorCtx.fillStyle = "rgba(32, 32, 32, 0.9)";
+    accumulatorCtx.fillRect(0, 0, this.vgaWidth, this.vgaHeight);
 
     accumulatorCtx.globalAlpha = 1.0;
     accumulatorCtx.drawImage(this.cleanSurface, 0, 0);
