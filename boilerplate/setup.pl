8d57aaa54747f3122a685e9ef9906600f2ac8ca4
Hevanafa <dhevansafa4@gmail.com>
2026-06-12 16:19:56 +0700

    added boilerplate setup prototype

---
+++ boilerplate/setup.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use v5.38.0;
+
+use Cwd qw(abs_path);
+use File::Copy qw(copy);
+use File::Spec::Functions qw(catfile);
+
+my $dest = abs_path(".");
+
+my $scripts_dir = abs_path("../scripts");
+my @scripts = ("clean.pl", "server.ts");
+
+for (@scripts) {
+  copy(catfile($scripts_dir, $_), $dest) or warn "Couldn't copy $_: $!";
+}
