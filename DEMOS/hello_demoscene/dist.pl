16533a24c284cde1a14c9e4d0ead229dbd3ae8ef
Hevanafa <dhevansafa4@gmail.com>
2026-06-12 19:26:31 +0700

    added dircopy use

--- DEMOS/hello_demoscene/dist.pl
+++ DEMOS/hello_demoscene/dist.pl
@@ -2,11 +2,18 @@ use strict;
 use warnings;
 use v5.38.0;
 
+use Cwd qw(abs_path);
 use File::Copy qw(copy);
-use File::Path qw(remove_tree);
+use File::Path qw(make_path remove_tree);
+# use File::Find qw(find);
+use File::Spec::Functions qw(catfile);
+
+# Requires:
+# cpanm File::Copy::Recursive
+use File::Copy::Recursive qw(dircopy);
 
 my $wasm = "game.wasm";
-my $dist_dir = "dist";
+my $dist_dir = abs_path("dist");
 
 unless (-f $wasm) {
   say "Missing $wasm!";
@@ -30,4 +37,19 @@ for (@files) {
   copy $_, $dist_dir
 }
 
-# TODO: Copy assets
+# Copy assets
+# find(sub {
+#   my $path = $File::Find::name;
+#
+#   my $relative = $path =~ s/^assets\///r;
+#   my $target = catfile("$dist_dir/assets", $relative);
+#   say $target;
+#
+#   if (-d) {
+#     make_path $target
+#   } else {
+#     copy($_, $target) or warn "Couldn't copy $relative: $!"
+#   }
+# }, "assets")
+
+dircopy("assets", "$dist_dir/assets") or warn "Couldn't copy assets: $!"
