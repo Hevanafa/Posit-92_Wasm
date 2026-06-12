0177805625b3ddd12f2466aec6bfe5d981715f94
Hevanafa <dhevansafa4@gmail.com>
2026-06-12 16:29:40 +0700

    added copy base files to dist

--- DEMOS/hello_demoscene/dist.pl
+++ DEMOS/hello_demoscene/dist.pl
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 use v5.38.0;
 
+use File::Copy qw(copy);
+use File::Path qw(remove_tree);
+
 my $wasm = "game.wasm";
 my $dist_dir = "dist";
 
@@ -14,5 +17,17 @@ remove_tree $dist_dir if -d $dist_dir;
 
 mkdir $dist_dir;
 
-# TODO: Copy base files
+# Copy base files
+my @files = (
+  $wasm,
+  "game.js",
+  "posit-92.js",
+  "index.html",
+  "favicon.ico"
+);
+
+for (@files) {
+  copy $_, $dist_dir
+}
+
 # TODO: Copy assets
