7b87e24dee6f2a7b4140a61f0fe3f51be8786015
Hevanafa <dhevansafa4@gmail.com>
2026-06-25 11:49:59 +0700

    added extra compiler options in make

--- snippets/utf8_strings/make.pl
+++ snippets/utf8_strings/make.pl
@@ -3,7 +3,7 @@ use warnings;
 use v5.38.2;
 
 my $exe_path = "E:/fpc-wasm/fpc/bin/x86_64-win64/fpc.exe";
-my @args = ("-Pwasm32", "-Tembedded");
+my @args = ("-Pwasm32", "-Tembedded", "-dWASM", "-Fu..\\..\\experimental\\units");
 my $main_file = "main.pas";
 
 my $exit_code = system $exe_path, @args, $main_file;
