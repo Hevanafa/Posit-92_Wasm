8f6717fcf4c86c4ebd0bb892d2e39c3ce6c08efb
Hevanafa <dhevansafa4@gmail.com>
2026-06-26 11:29:54 +0700

    added file size difference

--- scripts/optimise_wasm.pl
+++ scripts/optimise_wasm.pl
@@ -2,9 +2,11 @@ use strict;
 use warnings;
 use v5.38.2;
 
+use POSIX qw(ceil);
+
 my $wasm_opt_path = "E:/binaryen/bin/wasm-opt.exe";
 my $wasm_file = "game.wasm";
-my $output_file = "game_optimised.wasm";
+my $output_file = "game.wasm";
 
 unless (-e $wasm_opt_path) {
   say "Couldn't find wasm-opt at ".$wasm_opt_path;
@@ -15,6 +17,8 @@ unless (-e $wasm_file) {
   say "Missing $wasm_file!";
   exit 1
 }
+
+my $original_size = -s $wasm_file;
 
 my @args = (
   $wasm_file,
@@ -28,7 +32,11 @@ my @args = (
 system $wasm_opt_path, @args;
 
 if ($? != -1) {
-  say "Optimised $wasm_file successfully"
+  my $new_size = -s $output_file;
+
+  say "Optimised $wasm_file successfully";
+  # say "Size: " . $original_size . " --> " . $new_size
+  printf "Size: %d (%d KB) --> %d (%d KB)", $original_size, ceil($original_size / 1024), $new_size, ceil($new_size / 1024)
 } else {
   say "Failed to optimise."
 }
