9d7d484e92b6f43180b7f01365420bf6019b7a40
Hevanafa <dhevansafa4@gmail.com>
2026-06-26 15:52:27 +0700

    improved ensure script readability

--- scripts/ensure_engine_js.pl
+++ scripts/ensure_engine_js.pl
@@ -11,8 +11,10 @@ say "Checking engine JS...";
 my $engine_js_path = "../experimental/engine/posit-92.js";
 
 unless (-f $engine_js_path) {
+  my $cmd = "bun run make";
   say "Couldn't find the engine JS " . basename($engine_js_path);
+  say "Invoking $cmd...";
   
   chdir "../experimental/engine";
-  system "bun run make"
+  system $cmd or die "Couldn't execute $cmd: $!"
 }
