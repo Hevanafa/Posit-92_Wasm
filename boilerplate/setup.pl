0d8d8c58763e8e7be950a5cae69185fc3d0b554e
Hevanafa <dhevansafa4@gmail.com>
2026-06-26 15:37:11 +0700

    added boilerplate engine check call

--- boilerplate/setup.pl
+++ boilerplate/setup.pl
@@ -11,17 +11,9 @@ use Term::ANSIColor qw(colored);
 
 my $dest = abs_path(".");
 
-# Copy engine JS
-
-say "Checking engine JS...";
-
-my $engine_js_path = "../experimental/engine/posit-92.js";
-
-unless (-f $engine_js_path) {
-  say "Couldn't find the engine JS " . basename($engine_js_path);
-  
-  chdir "../experimental/engine";
-  system "bun run make"
+eval {
+  system "perl ../scripts/check_engine_js.pl";
+  1
 }
 
 chdir $dest;
--- scripts/check_engine_js.pl
+++ scripts/check_engine_js.pl
@@ -2,8 +2,14 @@ use strict;
 use warnings;
 use v5.38.2;
 
-unless (-f "../experimental/engine/posit-92.js") {
-  say "Couldn't find the engine JS posit-92.js";
+# Copy engine JS
+
+say "Checking engine JS...";
+
+my $engine_js_path = "../experimental/engine/posit-92.js";
+
+unless (-f $engine_js_path) {
+  say "Couldn't find the engine JS " . basename($engine_js_path);
   
   chdir "../experimental/engine";
   system "bun run make"
