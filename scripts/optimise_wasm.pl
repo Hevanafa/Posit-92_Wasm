6abd9b98fa33ad16279854e8fa8a80100dadf210
Hevanafa <dhevansafa4@gmail.com>
2026-06-26 11:25:50 +0700

    added optimise_wasm messages

--- scripts/optimise_wasm.pl
+++ scripts/optimise_wasm.pl
@@ -6,7 +6,12 @@ my $wasm_opt_path = "E:/binaryen/bin/wasm-opt.exe";
 my $wasm_file = "game.wasm";
 my $output_file = "game_optimised.wasm";
 
-if (-e $wasm_file) {
+unless (-e $wasm_opt_path) {
+  say "Couldn't find wasm-opt at ".$wasm_opt_path;
+  exit 1
+}
+
+unless (-e $wasm_file) {
   say "Missing $wasm_file!";
   exit 1
 }
@@ -20,4 +25,10 @@ my @args = (
   "--enable-bulk-memory"
 );
 
-system $wasm_opt_path, @args
+system $wasm_opt_path, @args;
+
+if ($? != -1) {
+  say "Optimised $wasm_file successfully"
+} else {
+  say "Failed to optimise."
+}
