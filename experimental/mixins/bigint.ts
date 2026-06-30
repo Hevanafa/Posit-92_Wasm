d7c3889774170887292e61b1235dfb50675ecab2
Hevanafa <dhevansafa4@gmail.com>
2026-06-30 13:13:14 +0700

    renamed method names to PascalCase

--- experimental/mixins/bigint.ts
+++ experimental/mixins/bigint.ts
@@ -9,14 +9,14 @@ class BigIntMixin extends Posit92 {
 
     Object.assign(env, {
       // BigInt
-      addBigInt: () => this.#AddBigInt.bind(this),
-      subtractBigInt: () => this.#SubtractBigInt.bind(this),
-      multiplyBigInt: () => this.#MultiplyBigInt.bind(this),
-      divideBigInt: () => this.#DivideBigInt.bind(this),
+      AddBigInt: () => this.#AddBigInt.bind(this),
+      SubtractBigInt: () => this.#SubtractBigInt.bind(this),
+      MultiplyBigInt: () => this.#MultiplyBigInt.bind(this),
+      DivideBigInt: () => this.#DivideBigInt.bind(this),
 
-      compareBigInt: () => this.#CompareBigInt.bind(this),
-      formatBigInt: () => this.#formatBigInt.bind(this),
-      formatBigIntScientific: () => this.#formatBigIntScientific.bind(this)
+      CompareBigInt: () => this.#CompareBigInt.bind(this),
+      FormatBigInt: () => this.#FormatBigInt.bind(this),
+      FormatBigIntScientific: () => this.#FormatBigIntScientific.bind(this)
     });
   }
 
@@ -34,7 +34,7 @@ class BigIntMixin extends Posit92 {
   /**
    * Pass a JS string to Pascal
    */
-  #loadStringBuffer(text): void {
+  #LoadStringBuffer(text): number {
     const encoder = new TextEncoder();
     const bytes = encoder.encode(text);
 
@@ -45,16 +45,16 @@ class BigIntMixin extends Posit92 {
     return bytes.length;
   }
 
-  #loadBigIntResult(n: bigint | string): void {
+  #LoadBigIntResult(n: bigint | string): void {
     if ((typeof n != "bigint") && (typeof n != "string"))
       throw new Error("n should be either of type BigInt or string");
 
-    const length = this.#loadStringBuffer(n.toString());
+    const length = this.#LoadStringBuffer(n.toString());
     const bufferPtr = this.WasmInstance.exports.getStringBuffer();
     this.WasmInstance.exports.loadBigIntResult(bufferPtr, length);
   }
 
-  #bufferPtrToString(bufferPtr: number): string {
+  #BufferPtrToString(bufferPtr: number): string {
     this.AssertNumber(bufferPtr);
 
     const buffer = new Uint8Array(this.WasmInstance.exports.memory.buffer, bufferPtr, 256);
@@ -69,10 +69,10 @@ class BigIntMixin extends Posit92 {
   // Begin arithmetic operations
 
   #AddBigInt(): void {
-    const biStrA = this.#bufferPtrToString(
-      this.wasmInstance.exports.getBigIntAPtr());
-    const biStrB = this.#bufferPtrToString(
-      this.wasmInstance.exports.getBigIntBPtr());
+    const biStrA = this.#BufferPtrToString(
+      this.WasmInstance.exports.getBigIntAPtr());
+    const biStrB = this.#BufferPtrToString(
+      this.WasmInstance.exports.getBigIntBPtr());
     
     const [a, b] = [BigInt(biStrA), BigInt(biStrB)];
 
@@ -80,111 +80,111 @@ class BigIntMixin extends Posit92 {
     // console.log("BigIntB", b)
     // console.log(a + b, (a + b).toString());
 
-    this.#loadBigIntResult(a + b);
+    this.#LoadBigIntResult(a + b);
   }
 
   #SubtractBigInt(): void {
-    const biStrA = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
-    const biStrB = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
+    const biStrA = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
+    const biStrB = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
     
     const [a, b] = [BigInt(biStrA), BigInt(biStrB)];
-    this.#loadBigIntResult(a - b);
+    this.#LoadBigIntResult(a - b);
   }
 
   #MultiplyBigInt(): void {
-    const biStrA = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
-    const biStrB = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
+    const biStrA = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
+    const biStrB = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
     
     const [a, b] = [BigInt(biStrA), BigInt(biStrB)];
-    this.#loadBigIntResult(a * b);
+    this.#LoadBigIntResult(a * b);
   }
 
   #DivideBigInt(): void {
-    const biStrA = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
-    const biStrB = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
+    const biStrA = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
+    const biStrB = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
     
     const [a, b] = [BigInt(biStrA), BigInt(biStrB)];
-    this.#loadBigIntResult(a / b);
+    this.#LoadBigIntResult(a / b);
   }
 
   #CompareBigInt(): void {
-    const biStrA = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
-    const biStrB = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
+    const biStrA = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
+    const biStrB = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntBPtr());
     
     const [a, b] = [BigInt(biStrA), BigInt(biStrB)];
 
     if (a > b)
-      this.#loadBigIntResult(1n);
+      this.#LoadBigIntResult(1n);
     else if (a < b)
-      this.#loadBigIntResult(-1n);
+      this.#LoadBigIntResult(-1n);
     else
-      this.#loadBigIntResult(0n);
+      this.#LoadBigIntResult(0n);
   }
 
-  #formatBigInt(): void {
-    const biStrA = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
+  #FormatBigInt(): void {
+    const biStrA = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
     const a = BigInt(biStrA);
     let readable = 0;
 
     if (a >= 10n ** 33n) {
       readable = Number(a / 10n ** 32n) / 10;
-      this.#loadBigIntResult(readable + "Dc");
+      this.#LoadBigIntResult(readable + "Dc");
 
     } else if (a >= 10n ** 30n) {
       readable = Number(a / 10n ** 29n) / 10;
-      this.#loadBigIntResult(readable + "No");
+      this.#LoadBigIntResult(readable + "No");
 
     } else if (a >= 10n ** 27n) {
       readable = Number(a / 10n ** 26n) / 10;
-      this.#loadBigIntResult(readable + "Oc");
+      this.#LoadBigIntResult(readable + "Oc");
 
     } else if (a >= 10n ** 24n) {
       readable = Number(a / 10n ** 23n) / 10;
-      this.#loadBigIntResult(readable + "Sp");
+      this.#LoadBigIntResult(readable + "Sp");
 
     } else if (a >= 10n ** 21n) {
       readable = Number(a / 10n ** 20n) / 10;
-      this.#loadBigIntResult(readable + "Sx");
+      this.#LoadBigIntResult(readable + "Sx");
 
     } else if (a >= 10n ** 18n) {
       readable = Number(a / 10n ** 17n) / 10;
-      this.#loadBigIntResult(readable + "Qi");
+      this.#LoadBigIntResult(readable + "Qi");
 
     } else if (a >= 10n ** 15n) {
       readable = Number(a / 10n ** 14n) / 10;
-      this.#loadBigIntResult(readable + "Qa");
+      this.#LoadBigIntResult(readable + "Qa");
     
     } else if (a >= 10n ** 12n) {
       readable = Number(a / 10n ** 11n) / 10;
-      this.#loadBigIntResult(readable + "T");
+      this.#LoadBigIntResult(readable + "T");
 
     } else if (a >= 1_000_000_000n) {
       readable = Number(a / 100_000_000n) / 10;
-      this.#loadBigIntResult(readable + "B");
+      this.#LoadBigIntResult(readable + "B");
       
     } else if (a >= 1_000_000n) {
       readable = Number(a / 100_000n) / 10;
-      this.#loadBigIntResult(readable + "M");
+      this.#LoadBigIntResult(readable + "M");
 
     } else if (a >= 1000n) {
       readable = Number(a / 100n) / 10;
-      this.#loadBigIntResult(readable + "K");
+      this.#LoadBigIntResult(readable + "K");
 
     } else
-      this.#loadBigIntResult(a);
+      this.#LoadBigIntResult(a);
   }
 
 
-  #formatBigIntScientific(): void {
-    const biStrA = this.#bufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
+  #FormatBigIntScientific(): void {
+    const biStrA = this.#BufferPtrToString(this.WasmInstance.exports.getBigIntAPtr());
     const a = BigInt(biStrA);
     const digits = a.toString().length;
 
     if (a >= 1000n) {
       const readable = Number(a / 10n ** (BigInt(digits) - 2n)) / 10;
-      this.#loadBigIntResult(readable + " * 10^" + (digits - 1));
+      this.#LoadBigIntResult(readable + " * 10^" + (digits - 1));
     } else
-      this.#loadBigIntResult(a);
+      this.#LoadBigIntResult(a);
   }
 
 }
