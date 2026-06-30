938dc876975a381c18310be76d3dc06ca7aabf83
Hevanafa <dhevansafa4@gmail.com>
2026-06-30 16:38:28 +0700

    added header metadata reader logic

--- DEMOS/read_demo_metadata.pl
+++ DEMOS/read_demo_metadata.pl
@@ -12,7 +12,42 @@ chdir $demo_dir;
 
 open my $fh, "<", "game.lpr";
 
-# TODO: Read the header
+my $line;
+my $skipped = 0;
+
+# while-loop only reads up to that line, while for-loop is greedy -- it consumes the whole file
+
+while ($line = <$fh>) {
+  $skipped++ if $line !~ /{/;
+
+  if ($skipped >= 10) {
+    close $fh;
+    say "Couldn't find any opening comments within the first 10 lines!";
+    exit
+  }
+
+  last if $line =~ /{/;
+}
+
+for $line (<$fh>) {
+  if ($line =~ /mixins:/i) {
+    chomp $line;
+    say $line;
+
+    my ($mixins) = $line =~ /[:](.*)/;
+    
+    my @mixins = map {
+      $_ =~ /^\s*(.+)\s*$/;
+      $1
+    } $mixins =~ /[^,]+/g;
+
+    say "Required mixins: ".(join " -- ", @mixins);
+
+    last
+  }
+
+  last if $line =~ /}/;
+}
 
 close $fh;
 
