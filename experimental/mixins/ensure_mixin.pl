f159799246a1655198f532cda3bfeb29d937792c
Hevanafa <dhevansafa4@gmail.com>
2026-07-01 10:57:34 +0700

    improved base filename check

--- experimental/mixins/ensure_mixin.pl
+++ experimental/mixins/ensure_mixin.pl
@@ -2,6 +2,8 @@ use strict;
 use warnings;
 use v5.38.2;
 
+use File::Basename qw(basename);
+
 my $mixin_name = $ARGV[0];
 
 unless ($mixin_name) {
@@ -13,8 +15,14 @@ unless ($mixin_name) {
   exit 1
 }
 
-my $source_file = $mixin_name;
-$source_file .= ".ts" if $mixin_name !~ /\.ts$/;
+# Normalise mixin name
+$mixin_name = basename($mixin_name);
+
+if ($mixin_name =~ /\.ts$/) {
+  ($mixin_name) = $mixin_name =~ /(.*)\.ts/
+}
+
+my $source_file = $mixin_name . ".ts";
 
 unless (-f $source_file) {
   say "Couldn't find ".$source_file."!";
