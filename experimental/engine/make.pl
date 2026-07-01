a330e42a24238279028be7d62d085c1199fb3715
Hevanafa <dhevansafa4@gmail.com>
2026-07-01 13:29:34 +0700

    updated engine make.pl with dir anchor

--- experimental/engine/make.pl
+++ experimental/engine/make.pl
@@ -2,6 +2,8 @@ use strict;
 use warnings;
 use v5.38.2;
 
+use FindBin qw($Bin);
+use File::Spec::Functions qw(catfile);
 use File::Which qw(which);
 
 unless (which "bun") {
@@ -10,4 +12,7 @@ unless (which "bun") {
   exit 1
 }
 
-system "bun build posit-92.ts --outdir ."
+my $script_dir = $Bin;
+my $engine_src = catfile($script_dir, "posit-92.ts");
+
+system "bun build $engine_src --outdir $script_dir"
