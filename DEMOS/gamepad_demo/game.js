452c3a947788bbb9c5cf288f506b91e907b5c364
Hevanafa <dhevansafa4@gmail.com>
2026-07-01 10:07:17 +0700

    reduced gamepad demo game boilerplate

--- DEMOS/gamepad_demo/game.js
+++ DEMOS/gamepad_demo/game.js
@@ -4,16 +4,6 @@
  * Experimental boilerplate without the intro
  */
 class Game extends GamepadMixin {
-  /**
-   * KeyboardEvent.code to DOS scancode
-   */
-  ScancodeMap = {
-    "Escape": 0x01,
-    "Space": 0x39,
-    "Enter": 0x1C,
-    // Add more scancodes as necessary
-  };
-
   AssetManifest = {
     images: {
       cursor: "assets/images/cursor.png"
@@ -24,72 +14,34 @@ class Game extends GamepadMixin {
     ])
   }
 
-  async loadDefaultFont() {
-    await this.loadBMFont(
-      "assets/fonts/nokia_cellphone_fc_8.txt",
-      this.wasmInstance.exports.defaultFontPtr(),
-      this.wasmInstance.exports.defaultFontGlyphsPtr());
-  }
-
   /**
    * @override
    */
-  async loadAssets() {
+  async LoadGameAssets() {
     let handle = 0;
 
-    this.initLoadingScreen();
+    this.InitLoadingScreen();
 
-    await this.loadImagesFromManifest(this.AssetManifest.images);
+    await this.LoadImagesFromManifest(this.AssetManifest.images);
     // Sounds can be loaded later
 
-    handle = await this.loadImage("assets/images/dosu_1.png");
-    this.wasmInstance.exports.setImgDosuEXE(handle, 0);
-    handle = await this.loadImage("assets/images/dosu_2.png");
-    this.wasmInstance.exports.setImgDosuEXE(handle, 1);
+    handle = await this.LoadImage("assets/images/dosu_1.png");
+    this.WasmInstance.exports.setImgDosuEXE(handle, 0);
+    handle = await this.LoadImage("assets/images/dosu_2.png");
+    this.WasmInstance.exports.setImgDosuEXE(handle, 1);
 
     // Add more assets as necessary
   }
 }
 
-const TargetFPS = 60;
-const FrameTime = 1000 / TargetFPS;
-/**
- * in milliseconds
- */
-let lastFrameTime = 0.0;
-
-var done = false;
-
-async function main() {
+async function Main() {
   const game = new Game("game");
-  await game.init();
-  await game.loadDefaultFont();
-
-  game.quickStart();
-
-  function loop(currentTime) {
-    if (done) {
-      game.cleanup();
-      return;
-    }
-
-    const elapsed = currentTime - lastFrameTime;
-
-    if (elapsed >= FrameTime) {
-      lastFrameTime = currentTime - (elapsed % FrameTime);  // Carry over extra time
-      game.update();
-      game.draw();
-    }
-
-    requestAnimationFrame(loop)
-  }
-
-  requestAnimationFrame(loop)
+  await game.Start();
 }
 
-function play() {
+function Play() {
   const overlay = document.getElementById("play-overlay");
   overlay.parentNode.removeChild(overlay);
 
-  main()
+  Main()
 }
