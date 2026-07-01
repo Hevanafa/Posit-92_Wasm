9f939f51a22ac1469b281ba948aff4e295705d49
Hevanafa <dhevansafa4@gmail.com>
2026-07-01 14:36:53 +0700

    updated optimise wasm with dir anchoring

--- scripts/optimise_wasm.pl
+++ scripts/optimise_wasm.pl
@@ -2,28 +2,36 @@ use strict;
 use warnings;
 use v5.38.2;
 
+use FindBin qw($Bin);
+use File::Spec::Functions qw(catfile);
+
 use POSIX qw(ceil);
 
+my $script_dir = $Bin;
 my $wasm_opt_path = "E:/binaryen/bin/wasm-opt.exe";
-my $wasm_file = "game.wasm";
-my $output_file = "game.wasm";
+
+my $game_wasm = "game.wasm";
+my $output_file = "game_optimised.wasm";
+
+my $wasm_path = catfile($script_dir, $game_wasm);
+my $output_path = catfile($script_dir, $output_file);
 
 unless (-e $wasm_opt_path) {
   say "Couldn't find wasm-opt at ".$wasm_opt_path;
   exit 1
 }
 
-unless (-e $wasm_file) {
-  say "Missing $wasm_file!";
+unless (-e $wasm_path) {
+  say "Missing $wasm_path!";
   exit 1
 }
 
-my $original_size = -s $wasm_file;
+my $original_size = -s $wasm_path;
 
 my @args = (
-  $wasm_file,
+  $wasm_path,
   "-o",
-  $output_file,
+  $output_path,
   "-Oz",
   "--strip-debug",
   "--enable-bulk-memory"
@@ -32,11 +40,15 @@ my @args = (
 system $wasm_opt_path, @args;
 
 if ($? != -1) {
-  my $new_size = -s $output_file;
+  my $new_size = -s $output_path;
 
-  say "Optimised $wasm_file successfully";
+  say "Optimised $game_wasm successfully";
   # say "Size: " . $original_size . " --> " . $new_size
-  printf "Size: %d (%d KB) --> %d (%d KB)", $original_size, ceil($original_size / 1024), $new_size, ceil($new_size / 1024)
+
+  printf(
+    "Size: %d (%d KB) --> %d (%d KB)",
+    $original_size, ceil($original_size / 1024),
+    $new_size, ceil($new_size / 1024))
 } else {
   say "Failed to optimise."
 }
