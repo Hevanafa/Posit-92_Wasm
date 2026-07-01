204aedcd114f95d212d08548648fef81dd3a7e4a
Hevanafa <dhevansafa4@gmail.com>
2026-07-01 09:44:47 +0700

    reduced sound demo Game boilerplate

--- DEMOS/sound/game.js
+++ DEMOS/sound/game.js
@@ -9,21 +9,6 @@ const
 
 // Game < SoundsMixin < Posit92
 class Game extends SoundsMixin {
-  /**
-   * KeyboardEvent.code to DOS scancode
-   */
-  ScancodeMap = {
-    "Escape": 0x01,
-    "Space": 0x39,
-
-    "Digit1": 0x02,
-    "Digit2": 0x03,
-    "Digit3": 0x04,
-    "Digit4": 0x05,
-    "Digit5": 0x06
-    // Add more scancodes as necessary
-  };
-
   AssetManifest = {
     images: {
       cursor: "assets/images/cursor.png",
@@ -41,53 +26,19 @@ class Game extends SoundsMixin {
     ])
   }
 
-  async LoadDefaultFont() {
-    await this.LoadBMFont(
-      "assets/fonts/nokia_cellphone_fc_8.txt",
-      this.WasmInstance.exports.DefaultFontPtr(),
-      this.WasmInstance.exports.DefaultFontGlyphsPtr());
-  }
-
-  async LoadAssets() {
+  /**
+   * @override
+   */
+  async LoadGameAssets() {
     this.InitLoadingScreen();
     await this.LoadImagesFromManifest(this.AssetManifest.images);
     await this.LoadSoundsFromManifest(this.AssetManifest.sounds);
   }
 }
 
-const TargetFPS = 60;
-const FrameTime = 1000 / TargetFPS;
-/**
- * in milliseconds
- */
-let lastFrameTime = 0.0;
-
-var done = false;
-
 async function Main() {
   const game = new Game("game");
-  await game.Init();
-  await game.LoadDefaultFont();
-  game.QuickStart();
-
-  function Loop(currentTime) {
-    if (done) {
-      game.Cleanup();
-      return;
-    }
-
-    const elapsed = currentTime - lastFrameTime;
-
-    if (elapsed >= FrameTime) {
-      lastFrameTime = currentTime - (elapsed % FrameTime);  // Carry over extra time
-      game.Update();
-      game.Draw();
-    }
-
-    requestAnimationFrame(Loop)
-  }
-
-  requestAnimationFrame(Loop)
+  await game.Start();
 }
 
 function Play() {
