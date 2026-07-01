f84b13c02afe23fc8e1a72f67d342c478ef2dfa7
Hevanafa <dhevansafa4@gmail.com>
2026-07-01 13:33:50 +0700

    updated clean script with dir anchor

--- scripts/clean.pl
+++ scripts/clean.pl
@@ -4,20 +4,24 @@ use v5.38.0;
 
 use FindBin qw($Bin);
 use File::Path qw(remove_tree);
-use File::Find;
+use File::Spec::Functions qw(catfile);
 
 # Folders to nuke
+
+my $script_dir = $Bin;
+
 for my $dir (qw(backup lib)) {
   next unless -d $dir;
-  remove_tree $dir, { verbose => 1 }
+  remove_tree catfile($script_dir, $dir), { verbose => 1 }
 }
 
 # Generated file extensions
+
 my @extensions = qw(wasm compiled o ppu);
 my $pattern = join("|", @extensions);
 
 for my $ext (@extensions) {
-  for (glob "*.$ext") {
+  for (glob catfile($script_dir, "*.$ext")) {
     unlink or warn "Could not delete $_: $!\n";
     say "Deleted $_"
   }
