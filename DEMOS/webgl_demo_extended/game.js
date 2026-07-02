0cad42dbcae10886de3946a963b947013b282b10
Hevanafa <dhevansafa4@gmail.com>
2026-07-02 12:18:29 +0700

    simplified game boilerplate

--- DEMOS/webgl_demo_extended/game.js
+++ DEMOS/webgl_demo_extended/game.js
@@ -20,46 +20,6 @@ class Game extends WebGLMixin {
 
     // Add more assets as necessary
   }
-
-  /**
-   * @returns {WebGLRenderingContext}
-   */
-  get gl() { return this.glCtx; }
-
-  /**
-   * @override
-   */
-  Draw() {
-    this.WasmInstance.exports.Draw();
-
-    const vertices = new Float32Array([
-      0.0, 0.5,
-      -0.5, -0.5,
-      0.5, -0.5
-    ]);
-
-    const buffer = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
-
-    const shaders = glsl`
-// vertex
-attribute vec2 aPosition;
-void main() {
-  gl_Position = vec4(aPosition, 0.0, 1.0);
-}
-
-precision mediump float;
-void main() {
-  // rgba
-  gl_FragColor = vec4(0.39, 0.58, 0.93, 1.0);
-}
-    `;
-
-    // TODO: Link the attribute
-    // TODO: call drawArrays
-
-  }
 }
 
 async function Main() {
