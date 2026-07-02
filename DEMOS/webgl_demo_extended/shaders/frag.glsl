7757b5093da8c1c9317aabe3c94f22a2cc5b3829
Hevanafa <dhevansafa4@gmail.com>
2026-07-02 12:18:39 +0700

    renamed glsl variables

--- DEMOS/webgl_demo_extended/shaders/frag.glsl
+++ DEMOS/webgl_demo_extended/shaders/frag.glsl
@@ -1,7 +1,7 @@
 precision mediump float;
 varying vec2 uv;
-uniform sampler2D tex;
+uniform sampler2D u_tex;
 
 void main() {
-  gl_FragColor = texture2D(tex, vec2(uv.x, 1.0 - uv.y));
+  gl_FragColor = texture2D(u_tex, vec2(uv.x, 1.0 - uv.y));
 }
--- DEMOS/webgl_demo_extended/shaders/vertex.glsl
+++ DEMOS/webgl_demo_extended/shaders/vertex.glsl
@@ -1,7 +1,7 @@
-attribute vec2 pos;
+attribute vec2 a_pos;
 varying vec2 uv;
 
 void main() {
-  uv = pos * 0.5 + 0.5;
-  gl_Position = vec4(pos, 0.0, 1.0);
+  uv = a_pos * 0.5 + 0.5;
+  gl_Position = vec4(a_pos, 0.0, 1.0);
 }
