f21351c9171dfd5e2c1f8c61b3c1b63942076fd6
Hevanafa <dhevansafa4@gmail.com>
2026-07-03 15:31:53 +0700

    added print CGA text

--- experimental/units/P92Core.pas
+++ experimental/units/P92Core.pas
@@ -1,5 +1,9 @@
 unit P92Core;
 
+{$Mode ObjFPC}
+{$H-}
+{$J+}
+
 interface
 
 type
@@ -14,16 +18,20 @@ var
 
 procedure InitEngine; public name 'InitEngine';
 procedure InitLoadingState; public name 'InitLoadingState';
+procedure SetCGAFontHandle(value: longint); public name 'SetCGAFontHandle';
 
 
 implementation
 
 uses
-  WasmMemMgr, Logger, InteropBuf, P92AssetRegistry, Timing
+  WasmMemMgr, Logger, InteropBuf, P92AssetRegistry, SoftwareTexDraw, Timing
 {$ifdef UseWebGL}
   , WebGL
 {$endif}
   ;
+
+var
+  cgaFontHandle: longint;
 
 procedure InitEngine;
 begin
@@ -44,6 +52,28 @@ procedure InitLoadingState;
 begin
   engineRunState := ersLoading;
   writelog('Entered loading state');
+end;
+
+procedure SetCGAFontHandle(value: longint);
+begin
+  cgaFontHandle := value
+end;
+
+procedure print(txt: string; x, y: smallint);
+var
+  c: char;
+  left: smallint;
+  row, col: smallint;
+begin
+  left := x;
+
+  for c in txt do begin
+    row := ord(c) div 16;
+    col := ord(c) mod 16;
+
+    SprRegion(cgaFontHandle, col * 8, row * 8, 8, 8, left, y);
+    inc(left, 8)
+  end;
 end;
 
 end.
