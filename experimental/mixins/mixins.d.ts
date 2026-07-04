8d684e0549ae191e6fdc35f29ec59cd44d1f3234
Hevanafa <dhevansafa4@gmail.com>
2026-07-04 18:29:39 +0700

    simplified class factory type

--- experimental/mixins/mixins.d.ts
+++ experimental/mixins/mixins.d.ts
@@ -1,3 +1,5 @@
-var BMFontMixin: <T extends Constructor<Posit92>>(Base: T) => Constructor & T;
-var BigIntMixin: <T extends Constructor<Posit92>>(Base: T) => Constructor & T; // was Constructor<IBigInt> & T
-var GamepadMixin: <T extends Constructor<Posit92>>(Base: T) => Constructor & T;
+type Posit92ClassFactory = <T extends Constructor<Posit92>>(Base: T) => Constructor & T;  // was Constructor<IBigInt> & T
+
+var BMFontMixin: Posit92ClassFactory;
+var BigIntMixin: Posit92ClassFactory;
+var GamepadMixin: Posit92ClassFactory;
