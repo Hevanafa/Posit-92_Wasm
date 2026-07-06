043ec73ba3cb5f0333cd11623ac245c8f480a3e9
Hevanafa <dhevansafa4@gmail.com>
2026-07-06 10:02:05 +0700

    removed asset boilerplate

--- DEMOS/chain_easing/assets.pas
+++ DEMOS/chain_easing/assets.pas
@@ -8,27 +8,6 @@ var
   imgCursor, imgBlinky: longint;
   imgDosuEXE: array[0..1] of longint;
 
-{ Asset boilerplate }
-procedure setImgCursor(const imgHandle: longint); public name 'setImgCursor';
-procedure setImgBlinky(const imgHandle: longint); public name 'setImgBlinky';
-procedure setImgDosuEXE(const imgHandle: longint; const idx: integer); public name 'setImgDosuEXE';
-
-
 implementation
-
-procedure setImgCursor(const imgHandle: longint);
-begin
-  imgCursor := imgHandle
-end;
-
-procedure setImgBlinky(const imgHandle: longint);
-begin
-  imgBlinky := imgHandle
-end;
-
-procedure setImgDosuEXE(const imgHandle: longint; const idx: integer);
-begin
-  imgDosuEXE[idx] := imgHandle
-end;
 
 end.
