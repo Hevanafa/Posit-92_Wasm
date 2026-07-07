42feecceac784e2d29f203d820e3512a5a8c7041
Hevanafa <dhevansafa4@gmail.com>
2026-07-07 10:36:48 +0700

    removed legacy asset manifest

--- DEMOS/sound_demo/game.js
+++ DEMOS/sound_demo/game.js
@@ -1,39 +1,7 @@
 "use strict";
 
-const
-  SfxBwonk = 1,
-  SfxBite = 2,
-  SfxBonk = 3,
-  SfxStrum = 4,
-  SfxSlip = 5;
-
 // Game < SoundMixin < Posit92
 class Game extends SoundMixin(BMFontMixin(Posit92)) {
-  AssetManifest = {
-    images: {
-      cursor: "assets/images/cursor.png",
-      dosu_exe: [
-        "assets/images/dosu_1.png",
-        "assets/images/dosu_2.png"
-      ]
-    },
-    sounds: new Map([
-      [SfxBwonk, "assets/sfx/bwonk.ogg"],
-      [SfxBite, "assets/sfx/bite.ogg"],
-      [SfxBonk, "assets/sfx/bonk.ogg"],
-      [SfxStrum, "assets/sfx/strum.ogg"],
-      [SfxSlip, "assets/sfx/slip.ogg"]
-    ])
-  }
-
-  /**
-   * @override
-   */
-  async LoadGameAssets() {
-    this.InitLoadingScreen();
-    await this.LoadImagesFromManifest(this.AssetManifest.images);
-    await this.LoadSoundsFromManifest(this.AssetManifest.sounds);
-  }
 }
 
 async function Main() {
