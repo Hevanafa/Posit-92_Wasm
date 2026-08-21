8e0efef72d988b7f515c2925cda5ba9b417ef8c0
Hevanafa <dhevansafa4@gmail.com>
2026-08-21 10:06:30 +0700

    added sync to DEMOS

--- fonts/sync_with_demos.pl
+++ fonts/sync_with_demos.pl
@@ -11,22 +11,51 @@ my @files = (
   "LICENSE.TXT"
 );
 
-# TODO: Copy to DEMOS
-# Copy to TESTS
+# List DEMOS dirs
+
+my $demos_dir = "../DEMOS";
+
+my $dh;
+
+opendir($dh, $demos_dir);
+
+my @demo_dirs = grep {
+  ($_ !~ /\.\.?/) && (-d catdir($demos_dir, $_))
+} readdir $dh;
+
+closedir $dh;
+
+# List TESTS dirs
 
 my $tests_dir = "../TESTS";
 
-opendir(my $dh, $tests_dir);
+opendir($dh, $tests_dir);
 
 my @test_dirs = grep {
-  ($_ !~ /\.\.?/) && (-d catfile($tests_dir, $_))
+  ($_ !~ /\.\.?/) && (-d catdir($tests_dir, $_))
 } readdir $dh;
+
+closedir($dh);
+
+# Copy to DEMOS
+
+for my $demo_path (@demo_dirs) {
+  for (@files) {
+    my $dest = catdir($demos_dir, $demo_path, "assets", "fonts");
+    my $dest_path = catfile($dest, $_);
+
+    say "Copying to ".$dest_path;
+    copy $_, $dest
+  }
+}
+
+# Copy to TESTS
 
 for my $proj_path (@test_dirs) {
   for (@files) {
     my $dest = catdir($tests_dir, $proj_path, "assets", "fonts");
-
     my $dest_path = catfile($dest, $_);
+
     say "Copying to ".$dest_path;
     copy $_, $dest
   }
