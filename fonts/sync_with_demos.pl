fa9e7cc9f126b4c70798589ea715516f8e4e1795
Hevanafa <dhevansafa4@gmail.com>
2026-08-21 09:58:29 +0700

    added copy font files prototype

--- fonts/sync_with_demos.pl
+++ fonts/sync_with_demos.pl
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 use v5.38.2;
 
+use File::Spec::Functions qw(catfile catdir);
+use File::Copy;
+
 my @files = (
   "p92_sans_11.png",
   "p92_sans_11.txt",
@@ -10,11 +13,17 @@ my @files = (
 
 # TODO: Copy to DEMOS
 # TODO: Copy to TESTS
-#
+
 my $tests_dir = "../TESTS";
 
-opendir my $dh, $tests_dir;
+opendir(my $dh, $tests_dir);
 
-say for grep {
-  ($_ !~ /\.\.?/) && (-d $tests_dir.$_)
+my @test_dirs = grep {
+  ($_ !~ /\.\.?/) && (-d catfile($tests_dir, $_))
 } readdir $dh;
+
+for my $proj_path (@test_dirs) {
+  for (@files) {
+    copy $_, catdir($proj_path, "assets", "fonts");
+  }
+}
