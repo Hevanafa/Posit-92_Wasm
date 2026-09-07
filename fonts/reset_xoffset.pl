09f2edd1e7592fc997f6be3e2d99a38d7f5316fd
Hevanafa <dhevansafa4@gmail.com>
2026-09-07 19:33:35 +0700

    added xoffset reset implementation

--- fonts/reset_xoffset.pl
+++ fonts/reset_xoffset.pl
@@ -3,11 +3,23 @@ use warnings;
 use v5.38.2;
 
 my $fh;
+my $font_def = "p92_sans_8.txt";
 
-open $fh, "<", "p92_sans_8.txt";
+open $fh, "<", $font_def;
 
 my @lines = <$fh>;
 
 close $fh;
 
-say for @lines;
+@lines = map {
+  chomp;
+  my $line = $_;
+  $line =~ s/xoffset=(-?\d)/xoffset=0/;
+  $line
+} @lines;
+
+open $fh, ">", $font_def;
+
+say $fh $_ for @lines;
+
+close $fh
