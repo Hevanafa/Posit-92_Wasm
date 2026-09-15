ee1301b1e1db178a44dc544d6c89d265eadc385e
Hevanafa <dhevansafa4@gmail.com>
2026-09-15 22:27:45 +0700

    updated version strings

--- experimental/engine/posit-92.ts
+++ experimental/engine/posit-92.ts
@@ -168,7 +168,7 @@ type Posit92Options = {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Posit92 {
-  static version = "0.3.1";
+  static version = "0.3.2_beta";
 
   readonly #DefaultVGAWidth = 320;
   readonly #DefaultVGAHeight = 200;
--- experimental/units/P92Core.pas
+++ experimental/units/P92Core.pas
@@ -7,7 +7,7 @@ unit P92Core;
 interface
 
 const
-  Posit92Version = '0.3.1';
+  Posit92Version = '0.3.2_beta';
 
 {$ifdef P92_SDL2}
 type
