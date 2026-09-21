d68d41d8bb22c323f2e43700e63ae6ef2fd390b8
Hevanafa <dhevansafa4@gmail.com>
2026-09-21 16:03:03 +0700

    removed legacy asset boilerplate

--- DEMOS/immedgui_prompt/assets.pas
+++ DEMOS/immedgui_prompt/assets.pas
@@ -7,141 +7,17 @@ interface
 uses P92AssetHandles;
 
 var
-  { for use in loadBMFont }
   defaultFont: TBMFontHandle;
-  defaultFontGlyphs: array[32..126] of TBMFontGlyph;
   blackFont: TBMFontHandle;
-  blackFontGlyphs: array[32..126] of TBMFontGlyph;
-
   picotronFont: TBMFontHandle;
-  picotronFontGlyphs: array[32..126] of TBMFontGlyph;
 
   imgCursor, imgHandCursor: longint;
   imgDosuEXE: array[0..1] of longint;
   imgWinNormal, imgWinHovered, imgWinPressed: longint;
   imgPromptBG, imgPromptButtonNormal, imgPromptButtonPressed: longint;
 
-{ BMFont boilerplate }
-function defaultFontPtr: PBMFont; public name 'defaultFontPtr';
-function defaultFontGlyphsPtr: PBMFontGlyph; public name 'defaultFontGlyphsPtr';
-function blackFontPtr: PBMFont; public name 'blackFontPtr';
-function blackFontGlyphsPtr: PBMFontGlyph; public name 'blackFontGlyphsPtr';
-
-function picotronFontPtr: PBMFont; public name 'picotronFontPtr';
-function picotronFontGlyphsPtr: PBMFontGlyph; public name 'picotronFontGlyphsPtr';
-
-procedure printDefault(const text: string; const x, y: integer);
-function measureDefault(const text: string): word;
-
-{ Asset boilerplate }
-procedure setImgCursor(const imgHandle: longint); public name 'setImgCursor';
-procedure setImgHand(const imgHandle: longint); public name 'setImgHand';
-procedure setImgDosuEXE(const imgHandle: longint; const idx: integer); public name 'setImgDosuEXE';
-
-procedure setImgWinNormal(const imgHandle: longint); public name 'setImgWinNormal';
-procedure setImgWinHovered(const imgHandle: longint); public name 'setImgWinHovered';
-procedure setImgWinPressed(const imgHandle: longint); public name 'setImgWinPressed';
-
-procedure setImgPromptBg(const imgHandle: longint); public name 'setImgPromptBg';
-procedure setImgBtnPromptNormal(const imgHandle: longint); public name 'setImgBtnPromptNormal';
-procedure setImgBtnPromptPressed(const imgHandle: longint); public name 'setImgBtnPromptPressed';
-
 
 implementation
-
-uses Conv;
-
-{ Begin BMFont boilerplate}
-
-function defaultFontPtr: PBMFont;
-begin
-  defaultFontPtr := @defaultFont
-end;
-
-function defaultFontGlyphsPtr: PBMFontGlyph;
-begin
-  defaultFontGlyphsPtr := @defaultFontGlyphs
-end;
-
-function blackFontPtr: PBMFont;
-begin
-  blackFontPtr := @blackFont
-end;
-
-function blackFontGlyphsPtr: PBMFontGlyph;
-begin
-  blackFontGlyphsPtr := @blackFontGlyphs
-end;
-
-function picotronFontPtr: PBMFont;
-begin
-  picotronFontPtr := @picotronFont
-end;
-
-function picotronFontGlyphsPtr: PBMFontGlyph;
-begin
-  picotronFontGlyphsPtr := @picotronFontGlyphs
-end;
-
-
-
-procedure printDefault(const text: string; const x, y: integer);
-begin
-  printBMFont(defaultFont, defaultFontGlyphs, text, x, y)
-end;
-
-function measureDefault(const text: string): word;
-begin
-  measureDefault := measureBMFont(defaultFont, defaultFontGlyphs, text)
-end;
-
-
-{ Begin asset boilerplate }
-
-procedure setImgCursor(const imgHandle: longint);
-begin
-  imgCursor := imgHandle
-end;
-
-procedure setImgHand(const imgHandle: longint);
-begin
-  imgHandCursor := imgHandle
-end;
-
-procedure setImgDosuEXE(const imgHandle: longint; const idx: integer);
-begin
-  imgDosuEXE[idx] := imgHandle
-end;
-
-procedure setImgWinNormal(const imgHandle: longint);
-begin
-  imgWinNormal := imgHandle
-end;
-
-procedure setImgWinHovered(const imgHandle: longint);
-begin
-  imgWinHovered := imgHandle
-end;
-
-procedure setImgWinPressed(const imgHandle: longint);
-begin
-  imgWinPressed := imgHandle
-end;
-
-procedure setImgPromptBg(const imgHandle: longint);
-begin
-  imgPromptBG := imgHandle
-end;
-
-procedure setImgBtnPromptNormal(const imgHandle: longint);
-begin
-  imgPromptButtonNormal := imgHandle
-end;
-
-procedure setImgBtnPromptPressed(const imgHandle: longint);
-begin
-  imgPromptButtonPressed := imgHandle
-end;
 
 
 end.
