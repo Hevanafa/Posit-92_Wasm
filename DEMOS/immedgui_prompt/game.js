2055b1add9e2522517fd4455160c736fc5272d8d
Hevanafa <dhevansafa4@gmail.com>
2026-09-21 16:30:43 +0700

    removed legacy boilerplate code

--- DEMOS/immedgui_prompt/game.js
+++ DEMOS/immedgui_prompt/game.js
@@ -1,29 +1,6 @@
 "use strict";
 
 class Game extends BMFontMixin(Posit92) {
-  async loadDefaultFont() {
-    await this.loadBMFont(
-      "assets/fonts/nokia_cellphone_fc_8.txt",
-      this.wasmInstance.exports.defaultFontPtr(),
-      this.wasmInstance.exports.defaultFontGlyphsPtr());
-  }
-
-  async loadAssets() {
-    let handle = 0;
-
-    this.initLoadingScreen();
-    await this.loadImagesFromManifest(this.AssetManifest.images);
-
-    await this.loadBMFont(
-      "assets/fonts/nokia_cellphone_fc_8.txt",
-      this.wasmInstance.exports.blackFontPtr(),
-      this.wasmInstance.exports.blackFontGlyphsPtr());
-
-    await this.loadBMFont(
-      "assets/fonts/picotron_8px.txt",
-      this.wasmInstance.exports.picotronFontPtr(),
-      this.wasmInstance.exports.picotronFontGlyphsPtr());
-  }
 }
 
 async function Main() {
