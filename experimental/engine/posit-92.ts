1e4ced50c7e6f28f21e811b68a3160e3d9eb0e49
Hevanafa <dhevansafa4@gmail.com>
2026-09-22 19:15:11 +0700

    deleted legacy manifest type defs

--- experimental/engine/posit-92.ts
+++ experimental/engine/posit-92.ts
@@ -5,12 +5,6 @@
 var debugEngineCalls = true;
 var debugRequests = true;
 
-// Legacy manifest typedefs
-
-type ImageManifest = Record<string, string | string[]>;
-type SoundManifest = Map<number, string>;
-type BMFontManifest = Map<string, { path: string, setter: string, glyphSetter: string }>;
-
 // Used by mixins
 type Constructor<T = {}> = new (...args: any[]) => T;
 
