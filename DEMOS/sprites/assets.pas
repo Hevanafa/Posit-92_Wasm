4f17ded607d0738b537f4027afd92f4b04cd25c1
Hevanafa <dhevansafa4@gmail.com>
2026-09-23 15:29:10 +0700

    removed legacy boilerplate

--- DEMOS/sprites/assets.pas
+++ DEMOS/sprites/assets.pas
@@ -1,89 +1,19 @@
 unit Assets;
 
-{$Mode TP}
+{$Mode ObjFPC}
+{$H+}{$J-}
 
 interface
 
-uses BMFont;
+uses P92AssetHandles;
 
 var
-  { for use in loadBMFont }
-  defaultFont: TBMFont;
-  defaultFontGlyphs: array[32..126] of TBMFontGlyph;
-
   imgCursor, imgHandCursor: longint;
   imgDosuEXE: array[0..1] of longint;
   imgSlimeGirl: longint;
   { https://kenney.nl/assets/desert-shooter-pack }
   imgBlueEnemy: longint;
 
-{ BMFont boilerplate }
-function defaultFontPtr: PBMFont; public name 'defaultFontPtr';
-function defaultFontGlyphsPtr: PBMFontGlyph; public name 'defaultFontGlyphsPtr';
-
-procedure printDefault(const text: string; const x, y: integer);
-function measureDefault(const text: string): word;
-
-{ Asset boilerplate }
-procedure setImgCursor(const imgHandle: longint); public name 'setImgCursor';
-procedure setImgHandCursor(const imgHandle: longint); public name 'setImgHandCursor';
-procedure setImgDosuEXE(const imgHandle: longint; const idx: integer); public name 'setImgDosuEXE';
-procedure setImgSlimeGirl(const imgHandle: longint); public name 'setImgSlimeGirl';
-procedure setImgBlueEnemy(const imgHandle: longint); public name 'setImgBlueEnemy';
-
-
 implementation
-
-uses Conv;
-
-{ Begin BMFont boilerplate}
-
-function defaultFontPtr: PBMFont;
-begin
-  defaultFontPtr := @defaultFont
-end;
-
-function defaultFontGlyphsPtr: PBMFontGlyph;
-begin
-  defaultFontGlyphsPtr := @defaultFontGlyphs
-end;
-
-procedure printDefault(const text: string; const x, y: integer);
-begin
-  printBMFont(defaultFont, defaultFontGlyphs, text, x, y)
-end;
-
-function measureDefault(const text: string): word;
-begin
-  measureDefault := measureBMFont(defaultFont, defaultFontGlyphs, text)
-end;
-
-
-{ Begin asset boilerplate }
-
-procedure setImgCursor(const imgHandle: longint);
-begin
-  imgCursor := imgHandle
-end;
-
-procedure setImgHandCursor(const imgHandle: longint);
-begin
-  imgHandCursor := imgHandle
-end;
-
-procedure setImgDosuEXE(const imgHandle: longint; const idx: integer);
-begin
-  imgDosuEXE[idx] := imgHandle
-end;
-
-procedure setImgSlimeGirl(const imgHandle: longint);
-begin
-  imgSlimeGirl := imgHandle
-end;
-
-procedure setImgBlueEnemy(const imgHandle: longint);
-begin
-  imgBlueEnemy := imgHandle
-end;
 
 end.
