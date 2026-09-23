02e02f355a95ab3b4b6460887b172e37fe0dd68e
Hevanafa <dhevansafa4@gmail.com>
2026-09-23 16:16:00 +0700

    removed legacy asset manifest

--- DEMOS/sprites/game.js
+++ DEMOS/sprites/game.js
@@ -1,14 +1,6 @@
 "use strict";
 
 class Game extends BMFontMixin(Posit92) {
-  AssetManifest = {
-    images: {
-      cursor: "assets/images/cursor.png",
-      hand_cursor: "assets/images/hand.png",
-      slime_girl: "assets/images/piyo_0426_slime_girl.png",
-      blue_enemy: "assets/images/blue_enemy.png"
-    }
-  }
 }
 
 async function Main() {
