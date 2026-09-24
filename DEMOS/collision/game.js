ad160abba1ddfc7d8d1943d8f39b3cbfec787d81
Hevanafa <dhevansafa4@gmail.com>
2026-09-24 14:50:39 +0700

    removed legacy boilerplate

--- DEMOS/collision/game.js
+++ DEMOS/collision/game.js
@@ -1,35 +1,15 @@
 "use strict";
 
 class Game extends BMFontMixin(Posit92) {
-  AssetManifest = {
-    images: {
-      cursor: "assets/images/cursor.png"
-      // Add more image assets here
-    }
-  }
-
-  async loadAssets() {
-    let handle = 0;
-
-    this.initLoadingScreen();
-
-    handle = await this.loadImage("assets/images/dosu_1.png");
-    this.wasmInstance.exports.setImgDosuEXE(handle, 0);
-    handle = await this.loadImage("assets/images/dosu_2.png");
-    this.wasmInstance.exports.setImgDosuEXE(handle, 1);
-
-    // Add more assets as necessary
-  }
 }
 
 async function Main() {
-  const game = new Game("game");
+  const game = new Game();
   await game.Start();
 }
 
 function Play() {
   const overlay = document.getElementById("play-overlay");
   overlay.parentNode.removeChild(overlay)
-
   Main()
 }
