3e82e48838901b2adcfdc63fbde9ea8012a3a3d5
Hevanafa <dhevansafa4@gmail.com>
2026-09-25 11:13:15 +0700

    removed legacy boilerplate

--- DEMOS/particles/game.js
+++ DEMOS/particles/game.js
@@ -1,84 +1,15 @@
 "use strict";
 
-class Game extends Posit92 {
-  /**
-   * KeyboardEvent.code to DOS scancode
-   */
-  ScancodeMap = {
-    "Escape": 0x01,
-    "Space": 0x39
-    // Add more scancodes as necessary
-  };
-
-  AssetManifest = {
-    images: {
-      cursor: "assets/images/cursor.png",
-      particle: "assets/images/particle.png"
-      // Add more image assets here
-    }
-  }
-
-  async loadDefaultFont() {
-    await this.loadBMFont(
-      "assets/fonts/nokia_cellphone_fc_8.txt",
-      this.wasmInstance.exports.defaultFontPtr(),
-      this.wasmInstance.exports.defaultFontGlyphsPtr());
-  }
-
-  async loadAssets() {
-    let handle = 0;
-
-    this.initLoadingScreen();
-    await this.loadImagesFromManifest(this.AssetManifest.images);
-
-    handle = await this.loadImage("assets/images/dosu_1.png");
-    this.wasmInstance.exports.setImgDosuEXE(handle, 0);
-    handle = await this.loadImage("assets/images/dosu_2.png");
-    this.wasmInstance.exports.setImgDosuEXE(handle, 1);
-
-    // Add more assets as necessary
-  }
+class Game extends BMFontMixin(Posit92) {
 }
 
-const TargetFPS = 60;
-const FrameTime = 1000 / TargetFPS;
-/**
- * in milliseconds
- */
-let lastFrameTime = 0.0;
-
-var done = false;
-
-async function main() {
-  const game = new Game("game");
-  await game.init();
-  await game.loadDefaultFont();
-
-  game.quickStart();
-
-  function loop(currentTime) {
-    if (done) {
-      game.cleanup();
-      return;
-    }
-
-    const elapsed = currentTime - lastFrameTime;
-
-    if (elapsed >= FrameTime) {
-      lastFrameTime = currentTime - (elapsed % FrameTime);  // Carry over extra time
-      game.update();
-      game.draw();
-    }
-
-    requestAnimationFrame(loop)
-  }
-
-  requestAnimationFrame(loop)
+async function Main() {
+  const game = new Game();
+  game.Start();
 }
 
-function play() {
+function Play() {
   const overlay = document.getElementById("play-overlay");
-  overlay.parentNode.removeChild(overlay)
-
-  main()
+  overlay.parentNode.removeChild(overlay);
+  Main()
 }
